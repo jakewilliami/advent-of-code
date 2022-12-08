0f26162f1d58859ed08ac540894c21816e2b146d
Jake Ireland <jakewilliami@icloud.com>
2022-12-08 23:59:01 +1300

    Improve AoC Day 8 2022 Julia solution regarding indices
    Much indexing code was copied from AoC 2020, day 11, and wasn't great
    in terms of generalisability, with certain functionality hard coded.
    In this commit, I generalise some of the N-dimensional indexing stuff,
    and I improve/simplify some logic in obtaining the global adjacencies.

--- 2022/day08/day08.jl
+++ 2022/day08/day08.jl
@@ -1,65 +1,72 @@
-const LEFT = (0, -1)
-const DOWN = (1, 0)
-const RIGHT = (0, 1)
-const UP = (-1, 0)
-
-const DIRECTIONS = CartesianIndex.((UP, DOWN, LEFT, RIGHT))
-
-
 # Parse input
 
-parse_input(f::String) = reduce(vcat, permutedims(parse.(Int, collect(s))) for s in readlines(f))
+parse_input(f::String) =
+    reduce(vcat, permutedims(parse.(Int, collect(s))) for s in readlines(f))
 
 
-# Part 1
+# Indexing stuff
 
-function tryindex(M::Matrix{T}, inds::NTuple{N, Int}...) where {T, N}
-    indices = Vector{Union{T, Nothing}}()
-
-    for idx in inds
-        try
-            push!(indices, getindex(M, idx...))
-        catch
-            push!(indices, nothing)
-        end
+function tryindex(M::Matrix{T}, i::CartesianIndex{N}) where {T, N}
+    try
+        return getindex(M, i)
+    catch
+        return nothing
     end
+end
+tryindices(M::Matrix{T}, is::CartesianIndex{N}...) where {T, N} =
+    Union{T, Nothing}[tryindex(M, i) for i in is]
 
-    return indices
+# Get a list of cartesian direction modifiers
+function cartesian_directions(dim::Int; exclude_diag::Bool = true)
+    T = Int
+    𝟎 = ntuple(_ -> zero(T), dim)
+    dir_itr = Base.Iterators.product([-one(T):one(T) for i in one(T):dim]...)
+
+    # The sum of the absolute values of the elements in a diagonal coordinate
+    # is always equal to the number of dimensions because, in a diagonal coordinate,
+    # all the elements have a value of either 1 or -1
+    fltr(t) = exclude_diag ? sum(abs.(t)) != dim : true
+
+    return CartesianIndex{dim}[CartesianIndex(t) for t in dir_itr if t ≠ 𝟎 && fltr(t)]
 end
 
-function adjacencies(M::Matrix{T}, idx::NTuple{N, Int}) where {T, N}
-    𝟎 = ntuple(_ -> zero(Int), N)
-    i = CartesianIndex(idx...)
-    return T[k for k in tryindex(M, ((i + j).I for j in DIRECTIONS)...) if !isnothing(k)]
-end
+# Increment an index further away from the origin, e.g., (1, -3) -> (2, -4)
+_move_further_from_origin(t::NTuple{N, T}) where {N, T} =
+    @. (abs(t) + 1) * sign(t)
+_move_further_from_origin(i::CartesianIndex{N}) where {N} =
+    CartesianIndex(_move_further_from_origin(i.I))
 
-
-function global_adjacencies(M::Matrix{T}, idx::NTuple{N, Int}) where {T, N}
-    D = Dict{CartesianIndex, Vector{T}}(s => T[] for s in DIRECTIONS)
+# Construct a map of lists of values in the given matrix at each direction away from a specified index
+function global_adjacencies(M::Matrix{T}, i::CartesianIndex{N}, direction_modifiers) where {T, N}
+    D = Dict{CartesianIndex, Vector{T}}(s => T[] for s in direction_modifiers)
 
     dᵢ = 1
     while dᵢ <= length(DIRECTIONS)
-        directional_shift = DIRECTIONS[dᵢ].I
+        directional_shift = DIRECTIONS[dᵢ]
+        j = i
         while true
-            adj_index = idx .+ directional_shift
+            j += directional_shift
 
-            if nothing ∈ tryindex(M, adj_index)
+            if isnothing(tryindex(M, j))
                 dᵢ += 1
                 break
             end
 
-            push!(D[DIRECTIONS[dᵢ]], M[adj_index...])
-            directional_shift = (abs.(directional_shift) .+ 1) .* sign.(directional_shift)
+            push!(D[DIRECTIONS[dᵢ]], M[j])
         end
     end
 
     return D
 end
 
+
+# Part 1
+
 function part1(map::Matrix{T}) where {T <: Number}
     res= 0
     for i in CartesianIndices(map)
-        A = global_adjacencies(map, i.I)
+        A = global_adjacencies(map, i, DIRECTIONS)
+        # Increment result if tree has a direct path in any cartesian direction
         res += any(isempty(A[d]) || all(map[i] > a for a in A[d]) for d in DIRECTIONS)
     end
     return res
@@ -72,9 +79,10 @@ function part2(map::Matrix{T}) where {T <: Number}
     scenic_scores = Vector{T}(undef, prod(size(map)))
 
     for (k, i) in enumerate(CartesianIndices(map))
+        A = global_adjacencies(map, i, DIRECTIONS)
+
         p = 1
         for d in DIRECTIONS
-            A = global_adjacencies(map, i.I)
             # Find the first tree that blocks our view
             j = findfirst(>=(map[i]), A[d])
             # If there is no such tree, we have the length of the whole row
@@ -90,6 +98,8 @@ end
 
 
 # Main
+
+const DIRECTIONS = cartesian_directions(2)
 
 function main()
     data = parse_input("data08.txt")
