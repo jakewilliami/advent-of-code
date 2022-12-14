791361ca0f19d18d559f349da4b7c1426f3ddb20
Jake Ireland <jakewilliami@icloud.com>
2022-12-14 21:31:33 +1300

    Clean up AoC Day 14 2022 Julia solution

--- 2022/day14/day14.jl
+++ 2022/day14/day14.jl
@@ -1,239 +1,190 @@
-function parse_input(data_file::String)
-    data = []
+# On each line of the puzzle input, we are given a list of (x, y) coordinates separated by
+# arrows.  Each of these coordinates represent the start and end of a range of rocks in a
+# cave.  The puzzle states that at a specified coordinate, sand is flowing into the cave,
+# and we are to model the falling sand given various constraints.  Sand will fall down if it
+# can (i.e., if no rocks or other sand is obstructing it), otherwise, it will try moving to
+# the bottom left, otherwise, the bottom right.  If it can't do any of that, it will stop
+# flowing and become sand at rest.
+#
+# In part one, we are to model the falling sand until it is falling out of the map.  The
+# result is the number of sand "units" that falls while this is happening.
+#
+# Part two states that there is an infinte floor of the cave, and we are to model the
+# falling sand until it stops flowing (again, counting the number of units this takes
+# for stability).
+#
+# In this puzzle, I once again make use of Julia's CartesianIndex, and CartesianIndices (to
+# represent the ranges from our puzzle input).  Instead of modelling a potentially large
+# matrix, I opted to use a dictionary whose keys were coordinates.
 
+
+### Parse input
+
+const RockRange = CartesianIndices{2, NTuple{2, UnitRange{Int}}}
+const RockRanges = Vector{RockRange}
+
+
+function parse_input(data_file::String)
+    data = RockRanges[]
+
+    # Each line represents a range of rocks
     for line in readlines(data_file)
-        C = []
+        C = CartesianIndex{2}[]
+
+        # For each node in range
         for p in split(line, " -> ")
             a_str, b_str = reverse(split(p, ","))
-            a, b = parse.(Int, (a_str, b_str))
+            a, b = parse(Int, a_str), parse(Int, b_str)
             push!(C, CartesianIndex(a, b))
         end
-        # C = [CartesianIndex(parse.(Int, reverse(split(p, ",")))...) for p in ps]
-        R = []
-        # println(C)
-        for i in 2:length(C)
+
+        R = RockRange[]
+
+        # For each pair in the nodes of ranges, construct an edge
+        # That is, construct a UnitRange from Cartesian indices
+        for i = 2:length(C)
             j, k = C[i - 1], C[i]
             r = j < k ? (j:k) : (k:j)
             push!(R, r)
         end
+
+        # Add list of rock ranges to data
         push!(data, R)
     end
 
     return data
 end
 
-function find_bounds(rocks)
-    rocks = Base.Iterators.flatten(rocks)
-    x2 = maximum(first(Tuple(i)) for i in rocks)
-    x1 = minimum(first(Tuple(i)) for i in rocks)
-    y2 = maximum(last(Tuple(i)) for i in rocks)
-    y1 = minimum(last(Tuple(i)) for i in rocks)
-    return CartesianIndex(x1, y1):CartesianIndex(x1, y2)
-    # return CartesianIndex(x1, y1), CartesianIndex(x2, y2)
+
+@enum CaveFeature rock sand_source sand_flowing sand_rest space
+
+const CaveMap = Dict{CartesianIndex{2}, CaveFeature}
+
+const SAND_SRC = CartesianIndex(0, 500)
+
+
+function construct_cave_map(data::Vector{RockRanges})
+    D = CaveMap(SAND_SRC => sand_source)
+
+    # Fill in the rocks in our cave map
+    for rock_ranges in data
+        for rock_range in rock_ranges
+            setindex!.(Ref(D), rock, rock_range)
+        end
+    end
+
+    return D
 end
 
-@enum Terrain rock sand_source sand_flowing sand_rest space
 
-function _flow_pos(i::CartesianIndex{2}, D::Dict{CartesianIndex{2}, Terrain})
-    # Try below
-    j = i + CartesianIndex(1, 0)
-    if !haskey(D, j) || D[j] == space
-        # println("    Can move down a D has no $j: $(get(D, j, nothing))")
-        return j
+### Part 1
+
+# If the index i doesn't exist in the cave map, or is empty space,
+# sand is allowed to flow there
+valid_sand_pos(i::CartesianIndex{2}, D::CaveMap) = !haskey(D, i) || D[i] == space
+
+
+# Model one iteration of flowing sand, based on the rules we were given
+function _flow!(i::CartesianIndex{2}, D::CaveMap)
+    # Try offsets in the following order: down, bottom left, bottom right
+    for d in (CartesianIndex(1, 0), CartesianIndex(1, -1), CartesianIndex(1, 1))
+        j = i + d
+        if valid_sand_pos(j, D)
+            D[j] = sand_flowing
+            return j
+        end
     end
 
-    # Try bottom left
-    j = i + CartesianIndex(1, -1)
-    if !haskey(D, j) || D[j] == space
-        # println("    Can move down left a D has no $j")
-        return j
-    end
-
-    # Try bottom right
-    j = i + CartesianIndex(1, 1)
-    if !haskey(D, j) || D[j] == space
-        # println("    Can move down right a D has no $j")
-        return j
-    end
-
-    return nothing
-end
-
-function _flow!(i::CartesianIndex{2}, D::Dict{CartesianIndex{2}, Terrain})
-    j = _flow_pos(i, D)
-    # println("Moving $i to $j")
-
-    if !isnothing(j)
-        D[j] = sand_flowing
-        return j
-    end
-
-    # Otherwise, sand at rest
+    # If none of the other positions are valid, the sand is at rest
     D[i] = sand_rest
     return i
 end
 
-function flow!(i::CartesianIndex{2}, D::Dict{CartesianIndex{2}, Terrain})
-    # Modify the state of all flowing sand
-    # Next unit of sand is not produced until the previous has come to rest
-    while D[i] != sand_rest
+
+# Modify the state of the cave map until the current unit of sand is at rest
+function flow!(i::CartesianIndex{2}, D::CaveMap)
+    while true
         j = i
         i = _flow!(i, D)
-        if D[i] != sand_rest
+
+        # If the sand is at rest, we are done
+        if D[i] == sand_rest
+            return i
+        else
+            # Otherwise, change the previous sand position into empty space
             D[j] = space
         end
     end
-    return i
-
-    # INGORE ME
-    for (i, t) in D
-        t == sand_flowing || continue
-        flow!(i, D)
-        D[i] = space
-    end
 end
 
-function find_the_botty(rocks)
-    rocks = Base.Iterators.flatten(rocks)
-    c2 = maximum(first(Tuple(i)) for i in rocks)
-    c1 = minimum(first(Tuple(i)) for i in rocks)
-    r2 = maximum(last(Tuple(i)) for i in rocks)
-    r1 = minimum(last(Tuple(i)) for i in rocks)
 
-    return CartesianIndex(r2, c1):CartesianIndex(r2, c2)
-end
+# The cave height is the largest y (row) coordinate in the map
+# This function assumes the map has been initialised with all rocks
+cave_height(D::CaveMap) = maximum(first(Tuple(i)) for i in keys(D))
 
-function find_height(rocks)
-    # rocks = Base.Iterators.flatten(rocks)
-    h = 0
-    for rock_ranges in rocks
-        for rock_range in rock_ranges
-            for rock_i in rock_range
-                h′ = first(Tuple(rock_i))
-                if h′ > h
-                    h = h′
-                end
-            end
-        end
-    end
-    return h + 1
-    println(collect(rocks))
-    return maximum(last(Tuple(i)) for i in rocks) + 1
-end
 
-# botty::CartesianIndices{2, Tuple{UnitRange{Int64}, UnitRange{Int64}}}
-function sand_will_fall_into_abyss(D::Dict{CartesianIndex{2}, Terrain}, height, src::CartesianIndex{2})
-    # Sand will always go down unless at rest, so we can just count if the
-    # sand has flowed more than the height
+function sand_will_fall_into_abyss(D::CaveMap, height::Int)
     D = deepcopy(D)
-    c = 1
-    i = src
-    # TODO: check if i needs to be set _below_ src
-    # while first(Tuple(i)) < first(Tuple(first(botty))) && last(Tuple(i)) < last(Tuple(last(botty)))
-    # println(height)
+    i, c = SAND_SRC, 0
+
+    # Sand will always go down unless at rest, so we can just count if the
+    # sand has flowed more than the height of the cave
     while D[i] != sand_rest
-        # i = flow!(i, D)
         i = _flow!(i, D)
-        # println(c, " ", c <= height)
         c += 1
+
+        # If the number of positions the sand has fallen is greater than the
+        # total height of the cave, the sand is falling into the abyss!
         if c > height
-            # println(D)
             return true
         end
     end
 
-    # if c > height
-        # return true
-    # end
-
-    @assert D[i] == sand_rest
     return false
 end
 
-function main(data)
-    # max_, min_ = find_bounds(data)
-    src = CartesianIndex(0, 500)
-    # Construct initial state
-    D = Dict{CartesianIndex{2}, Terrain}(src => sand_source)
-    for rock_ranges in data
-        for rock_range in rock_ranges
-            # println(rock_range)
-            for rock_i in rock_range
-                D[rock_i] = rock
-            end
-        end
-    end
 
+function part1(data::Vector{RockRanges})
+    D = construct_cave_map(data)
+    h = cave_height(D)
 
-    H = find_height(data)
-    # println(D)
-    # return sand_will_fall_into_abyss(D, H, src)
     res = 0
-    while !sand_will_fall_into_abyss(D, H, src)
-        _i = flow!(src, D)
+
+    # Fill up the cave until sand is flowing into the abyss
+    while !sand_will_fall_into_abyss(D, h)
+        flow!(SAND_SRC, D)
         res += 1
     end
-    return res
-    return find_height(data)
-    return find_the_botty(data)
 
-    # Make sand flow
-
-    while true
-
-    end
-end
-
-# println(main(data))
-part1(data) = main(data)
-
-
-function main2(data)
-    # max_, min_ = find_bounds(data)
-    src = CartesianIndex(0, 500)
-    # Construct initial state
-    D = Dict{CartesianIndex{2}, Terrain}(src => sand_source)
-    for rock_ranges in data
-        for rock_range in rock_ranges
-            # println(rock_range)
-            for rock_i in rock_range
-                D[rock_i] = rock
-            end
-        end
-    end
-
-    H = find_height(data)
-
-    # Make floor
-    for i in -1000:1000
-        D[CartesianIndex(H + 1, i)] = rock
-    end
-
-    # Let sand flow
-    res = 0
-    # i = src
-    i = CartesianIndex(0)
-    # while get(D, i, space) != sand_rest
-    while i != src
-        i = flow!(src, D)
-        # while D[i] != sand_rest
-            # j = i
-            # i = _flow!(i, D)
-            # if D[i] != sand_rest
-                # D[j] = space
-            # end
-        # end
-        res += 1
-    end
     return res
 end
 
-# println(main2(data))
-part2(data) = main2(data)
 
+### Part 2
+
+function part2(data::Vector{RockRanges})
+    D = construct_cave_map(data)
+    h = cave_height(D)
+
+    # Make floor (floor level is 2 more than the height of the cave)
+    setindex!.(Ref(D), rock, CartesianIndex(h + 2, -1000):CartesianIndex(h + 2, 1000))
+
+    # Let sand flow all across the floor, until full
+    res = 0
+    i = CartesianIndex{2}()
+    while i != SAND_SRC
+        i = flow!(SAND_SRC, D)
+        res += 1
+    end
+
+    return res
+end
+
+
+# Main
 
 function main()
-    data = parse_input("data13.txt")
+    data = parse_input("data14.txt")
 
     # Part 1
     part1_solution = part1(data)
