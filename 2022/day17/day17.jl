fb8800f053bf6d600a4ed86ad15ab8a869095a31
Jake Ireland <jakewilliami@icloud.com>
2022-12-18 02:33:14 +1300

    Solve AoC Day 17 2022 part 1 in Julia

--- 2022/day17/day17.jl
+++ 2022/day17/day17.jl
@@ -3,7 +3,7 @@ using AdventOfCode.Multidimensional
 using DataStructures
 
 f = "data17.txt"
-f = "test.txt"
+# f = "test.txt"
 
 data = strip(read(f, String))
 
@@ -95,13 +95,16 @@ function highest_rock(chamber)
 end
 
 function chamber_height(chamber)
-    w = size(chamber, 1)
+    h = size(chamber, 1)
     # Subtract one to account for the floor;
     # Decrement again once to account for the first _empty_ row (we want the previous one from that)
-    return findfirst(!any(chamber[w - i + 1, :]) for i in 1:w) - 1 - 1
+    return findfirst(!any(chamber[h - i + 1, :]) for i in 1:h) - 1 - 1
 end
 
 # setindex!.(Ref(A), 1, ROCKS[i].points)
+
+rock_obstructed(chamber::Matrix{Bool}, rock_indices::Stack{CartesianIndex{2}}, offset::CartesianIndex{2}) =
+    any(chamber[p + offset] for p in rock_indices if (p + offset) ∉ rock_indices)
 
 # Updates the rock position in the chamber matrix, and the rock indices stack
 function update_rock_position!(chamber::Matrix{Bool}, rock_indices::Stack{CartesianIndex{2}}, offset::CartesianIndex{2})
@@ -111,7 +114,8 @@ function update_rock_position!(chamber::Matrix{Bool}, rock_indices::Stack{Cartes
 
     # Check that there is not another rock in the way!
     # offset_indices = CartesianIndex{2}[p + offset for p in rock_indices]
-    move_allowed = all(!chamber[p + offset] for p in rock_indices if (p + offset) ∉ rock_indices)
+    # move_allowed = all(!chamber[p + offset] for p in rock_indices if (p + offset) ∉ rock_indices)
+    move_allowed = !rock_obstructed(chamber, rock_indices, offset)
     # move_allowed = all(!chamber[p + offset] for p in rock_indices if (p + offset) ∉ offset_indices)
     move_allowed || return chamber
 
@@ -134,14 +138,22 @@ end
 
 function draw(mat::Matrix{Bool})
     io = IOBuffer()
+    diagram_started = false
     for (i, r) in enumerate(eachrow(mat))
         i == size(mat, 1) && continue  # ignore floor
+        # ignore empty leading rows
+        !any(r) && !diagram_started && continue
+        diagram_started = true
+
+        # draw diagram
         print(io, '|')
         for c in r
             print(io, c ? '#' : '.')#print(io, c ? '█' : ' ')
         end
         println(io, '|')
     end
+
+    # draw floor
     print(io, '+')
     for _ in 1:size(mat, 2)
         print(io, '-')
@@ -154,24 +166,24 @@ end
 
 # TODO: optimisation: ignore all below row that is fulled up
 function main(data)
-    chamber = zeros(Bool, 30, 7)
+    chamber = zeros(Bool, 4000, 7)
     chamber[size(chamber, 1), :] .= true  # floor
     i, rᵢ = 0, 1
     rock_falling = false
     previous_rock_points = Stack{CartesianIndex{2}}()
     # prev_j = highest_rock(chamber)
 
-    while true
+    while rᵢ <= 2022 || rock_falling
         i  += 1
         j = highest_rock(chamber)
-        ;;println("i: $i (rᵢ = $rᵢ => mod1(rᵢ, length(ROCKS))), j: $j")
+        ####println("i: $i (rᵢ = $rᵢ => mod1(rᵢ, length(ROCKS))), j: $j")
         # Alternate between falling and getting pushed by hot gas
         if !iszero(mod(i, 2))
             # rock is falling
-            ;;println("Rock is falling")
+            ####println("Rock is falling")
             if rock_falling
                 # rock is still falling
-                println("  Rock is falling")
+                ####println("  Rock is falling")
                 # if j == prev_j
                 # if !rock_falling
                     # then rock has finished falling
@@ -183,59 +195,65 @@ function main(data)
                     # update rock position
                     update_rock_position!(chamber, previous_rock_points, CartesianIndex(1, 0))
                     # foreach(println, eachrow(chamber))
-                    ;;draw(chamber)
+                    ####draw(chamber)
                 # end
             else
                 # prev_j = j
                 # @label new_rock
-                ;;println("  Rock has only just begun falling: ")
+                ####println("  Rock has only just begun falling: ")
                 # a new rock needs to start falling
                 # rᵢ += 1
                 rock = ROCKS[mod1(rᵢ, length(ROCKS))]
                 # println(findfirst(any, eachrow(chamber)))
                 j = highest_rock(chamber)
-                ;;println("j: $j; rock: $rock")
+                ####println("j: $j; rock: $rock")
                 # rock begins falling 2 from left wall, and 3 above bottom
                 # draw initial rock state
                 empty!(previous_rock_points)
                 for p in rock.points
                     p′ = p + CartesianIndex(j - rock.height - 3 - 1, 2)
-                    ###println(p, " ", p′)
+                    # p′ = p + CartesianIndex(size(chamber, 1) - chamber_height(chamber) - rock.height - 3 - 1, 2)  # equivalent to above
+                    ####println(p, " ", p′, " ($(size(chamber, 1)), $(chamber_height(chamber)))")
                     chamber[p′] = true
                     push!(previous_rock_points, p′)
                 end
                 rock_falling = true
                 # foreach(println, eachrow(chamber))
-                ;;draw(chamber)
+                ####draw(chamber)
             end
         else
             #foreach(println, eachrow(chamber))
             # prev_j = j
             # rock is being pushed
             c = data[mod1(cld(i, 2), length(data))]
-            ;;println("Rock is being pushed: '$c':")
+            ####println("Rock is being pushed: '$c':")
             k = c == '<' ? CartesianIndex(0, -1) : CartesianIndex(0, 1)
             # update rock position
             update_rock_position!(chamber, previous_rock_points, k)
             # foreach(println, eachrow(chamber))
-            ;;draw(chamber)
+            ####draw(chamber)
             # if prev_j == j
             # if highest_rock(chamber) == prev_j + 1
             # j = highest_rock(chamber)
             # q = lowest_point(rock)
+            # Lowest point in the current rock
             q = minimum(first(Tuple(p)) for p in previous_rock_points)
+            # The set of horizontal coordinates we should check isn't being obstructed
             xs = Set{Int}(last(Tuple(p)) for p in previous_rock_points)
             rock = ROCKS[mod1(rᵢ, length(ROCKS))]
-            ;;println("$previous_rock_points")
+            ####println("$previous_rock_points")
             # println("row $([first(Tuple(p)) + q for p in previous_rock_points])")
             # println("HERE: row $([first(Tuple(p)) + q for p in previous_rock_points]): $([chamber[p + CartesianIndex(q, 0)] for p in previous_rock_points])")
-            ;;println("HERE: row $([(q + rock.height, x) for x in xs]): $([chamber[q + rock.height, x] for x in xs])")
-            if any(chamber[q + rock.height, x] for x in xs)
+            ####println("HERE: row $([(q + rock.height, x) for x in xs]): $([chamber[q + rock.height, x] for x in xs])")
+            # if any(chamber[q + rock.height, x] for x in xs)
+            offset = CartesianIndex(1, 0)
+            # if any(chamber[p + offset] for p in previous_rock_points if (p + offset) ∉ previous_rock_points)
+            if rock_obstructed(chamber, previous_rock_points, CartesianIndex(1, 0))
             # if any(chamber[j + rock.height, :])
             # if any(chamber[j + 1, p] for p in 1:size(chamber, 2))
             # if any(chamber[highest_rock(chamber) + 1, :])  # If the next row is at all full,
-                ;;println("Rock has come to rest")
-                ;;println("OK: j + 1 = $(j + 1), highest rock = $(highest_rock(chamber))")
+                ####println("Rock has come to rest")
+                ####println("OK: j + 1 = $(j + 1), highest rock = $(highest_rock(chamber))")
                 rock_falling = false
                 rᵢ += 1
                 # i -= 1
@@ -247,9 +265,9 @@ function main(data)
             # prev_j = j
         # end
 
-        if rᵢ >= 14 && !rock_falling
-            break
-        end
+        # if  rᵢ >= 20 && !rock_falling
+            # break
+        # end
     end
     # println(chamber)
     draw(chamber)
@@ -261,4 +279,5 @@ function main(data)
     # return size(chamber, 1) - highest_rock(chamber)
 end
 
+# 3207 TOO HIGH
 println(main(data))
