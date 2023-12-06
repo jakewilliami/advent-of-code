ea514d8c1064e41370a75a87f25214ad8540ed04
Jake Ireland <jakewilliami@icloud.com>
2023-12-06 14:58:11 +1300

    Further clean up for AoC Day 5 2023 in Julia

--- 2023/day05/day05.jl
+++ 2023/day05/day05.jl
@@ -7,7 +7,7 @@ struct IntervalRange
     dst::ClosedInterval
 end
 
-struct HortMap
+struct HorticultureMap
     desc::AbstractString
     maps::Vector{IntervalRange}
 end
@@ -17,7 +17,7 @@ function parse_input(input_file::String)
     seeds_str = popfirst!(A)
     seeds = [parse(Int, a) for a in split(last(split(seeds_str, ": ")))]
 
-    B = HortMap[]
+    B = HorticultureMap[]
     for a in A
         b = split(a, "\n")
         X = []
@@ -29,14 +29,13 @@ function parse_input(input_file::String)
                 push!(X, IntervalRange(i1, i2))
             end
         end
-        push!(B, HortMap(b[1][1:end-1], X))
+        push!(B, HorticultureMap(b[1][1:end-1], X))
     end
 
-    # return re_parse_data((seeds, B))
     return seeds, B
 end
 
-function get_val(M::HortMap, n::Int)
+function src_to_dst(M::HorticultureMap, n::Int)
     for m in M.maps
         if n ∈ m.src
             return leftendpoint(m.dst) + (n - leftendpoint(m.src))
@@ -45,34 +44,22 @@ function get_val(M::HortMap, n::Int)
     return n
 end
 
-function process_values(M::HortMap, values)
-
-    V = []
-    for v in values
-        push!(V, get_val(M, v))
+function src_to_dst!(V::Vector{Int}, M::HorticultureMap)
+    for i in eachindex(V)
+        V[i] = src_to_dst(M, V[i])
     end
     return V
 end
 
-function part1(data)
-    seeds, A = data
-    V = seeds
-    for a in A
-        V = process_values(a, V)
+function part1(seeds::Vector{Int}, M::Vector{HorticultureMap})
+    for m in M
+        src_to_dst!(seeds, m)
     end
-    return minimum(V)
-
-
-
-    V, A = data
-    for a in A
-        V = process_values(a, V)
-    end
-    return minimum(V)
+    return minimum(seeds)
 end
 
-function parse_seeds(seeds)
-    A = []
+function parse_seeds(seeds::Vector{Int})
+    A = ClosedInterval[]
     for i in 1:2:(length(seeds)-1)
         j = seeds[i]..(seeds[i]+seeds[i+1]-1)
         push!(A, j)
@@ -80,27 +67,13 @@ function parse_seeds(seeds)
     return A
 end
 
-function src_to_dst(M::HortMap, n::Int)
+function process_values!(R::Vector{ClosedInterval}, M::HorticultureMap)
+    A = ClosedInterval[]
     for m in M.maps
-        if n ∈ m.src
-            return leftendpoint(m.dst) + (n - leftendpoint(m.src)) + 1
-        end
-    end
-    return n
-end
-
-function interval_except(i1, i2)
-    return leftendpoint(i1)..leftendpoint(i2), rightendpoint(i2)..rightendpoint(i1)
-end
-
-function process_values!(R, M)
-    A = []
-    for m in M.maps
-        NR = []
+        NR = ClosedInterval[]
         while length(R) > 0
             r = pop!(R)
             i = r ∩ m.src
-            # dst = src_to_dst(M, leftendpoint(i)) .. src_to_dst(M, rightendpoint(i))
             before = leftendpoint(r)..min(rightendpoint(r), leftendpoint(m.src))
             after = max(rightendpoint(m.src), leftendpoint(r)) .. rightendpoint(r)
             width(before) > 0 && push!(NR, before)
@@ -111,16 +84,14 @@ function process_values!(R, M)
         append!(R, NR)
     end
     append!(R, A)
+    return R
 end
 
-function part2(data)
-    seeds, M = data
+function part2(seeds::Vector{Int}, M::Vector{HorticultureMap})
     seeds = parse_seeds(seeds)
-
-
-    S = []
+    S = Int[]
     for s in seeds
-        R = [s]
+        R = ClosedInterval[s]
         for m in M
             process_values!(R, m)
         end
@@ -130,15 +101,15 @@ function part2(data)
 end
 
 function main()
-    data = parse_input("data05.txt")
+    seeds, horticulture_maps = parse_input("data05.txt")
 
     # Part 1
-    part1_solution = part1(data)
+    part1_solution = part1(copy(seeds), horticulture_maps)
     @assert part1_solution == 107430936
     println("Part 1: $part1_solution")
 
     # Part 2
-    part2_solution = part2(data)
+    part2_solution = part2(copy(seeds), horticulture_maps)
     @assert part2_solution == 23738616
     println("Part 2: $part2_solution")
 end
