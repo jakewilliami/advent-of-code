3fe5fdfb945954d7eeccc3bb8420f658aa3abc53
Jake Ireland <jakewilliami@icloud.com>
2023-12-09 18:42:14 +1300

    Clean up AoC Day 9 2023 Julia solution

--- 2023/day09/day09.jl
+++ 2023/day09/day09.jl
@@ -1,26 +1,14 @@
-# using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+const Row{T} = Vector{T}
+const Table{T} = Vector{Row{T}}
 
-function parse_input(input_file::String)
-    # L = get_integers.(readlines(input_file))
-    L = [parse.(Int, split(l)) for l in readlines(input_file)]
-    return L
-    return collect(zip(L, gcd.(L)))
-end
+parse_input(input_file::String) = Row{Int}[parse.(Int, split(l)) for l in readlines(input_file)]
 
-function calc_new_row(row)
-    return [row[i] - row[i - 1] for i in 2:length(row)]
-    return eltype(row)[row[i] - row[i - 1] for i in 2:length(row)]
-end
 
-function fill_table(row)
+### Part 1 ###
+
+calc_new_row(row::Row{T}) where {T <: Number} = T[row[i] - row[i - 1] for i in 2:length(row)]
+
+function fill_table(row::Row{T}) where {T <: Number}
     table = [row]
     while !all(iszero, row)
         row = calc_new_row(row)
@@ -29,60 +17,48 @@ function fill_table(row)
     return table
 end
 
-function extrapolate_table(table)
+function extrapolate_table(table::Table{T}) where {T <: Number}
     table = deepcopy(table)
     push!(table[end], 0)
     for i in (length(table) - 1):-1:1
-        a = table[i][end]
-        c = table[i + 1][end]
-        # println(i, " ", a, " ",c, " ",  table[i], table[i + 1])
-        b = c + a
-        push!(table[i], b)
+        push!(table[i], table[i][end] + table[i + 1][end])
     end
     return table
 end
 
-function extrapolate_backwards(table)
-    table = deepcopy(table)
-    pushfirst!(table[end], 0)
-    for i in (length(table) - 1):-1:1
-        b = table[i][1]
-        c = table[i + 1][1]
-        # println(i, " ", a, " ",c, " ",  table[i], table[i + 1])
-        a = b - c
-        pushfirst!(table[i], a)
-    end
-    return table
-end
-
-function first_extrapolation(row)
+function first_extrapolation(row::Row{T}) where {T <: Number}
     full_table = fill_table(row)
     extrapolated = extrapolate_table(full_table)
     return last(first(extrapolated))
 end
 
-function part1(data)
-    return sum(first_extrapolation(row) for row in data)
+part1(data::Table{Int}) = sum(first_extrapolation(row) for row in data)
+
+
+### Part 2 ###
+
+function extrapolate_table_backwards(table::Table{T}) where {T <: Number}
+    table = deepcopy(table)
+    pushfirst!(table[end], 0)
+    for i in (length(table) - 1):-1:1
+        pushfirst!(table[i], table[i][1] - table[i + 1][1])
+    end
+    return table
 end
 
-function part2(data)
-    # println(extrapolate_backwards(fill_table(data[3])))
-    # return 0
-    res = 0
-    for row in data
-        full_table = fill_table(row)
-        # extrapolated = extrapolate_table(full_table)
-        # res += last(first(extrapolated))
-        extr_back = extrapolate_backwards(full_table)
-        res += first(first(extr_back))
-    end
-    return res
+function first_back_extrapolation(row::Row{T}) where {T <: Number}
+    full_table = fill_table(row)
+    extrapolated = extrapolate_table_backwards(full_table)
+    return first(first(extrapolated))
 end
+
+part2(data::Table{Int}) = sum(first_back_extrapolation(row) for row in data)
+
+
+### Main ###
 
 function main()
     data = parse_input("data09.txt")
-    # data = parse_input("data09.test.txt")
-    # println(data)
 
     # Part 1
     part1_solution = part1(data)
@@ -96,5 +72,3 @@ function main()
 end
 
 main()
-
-# not -328445473
