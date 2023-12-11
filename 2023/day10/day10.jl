640084b510948890f38412299620746d8d72d5cb
Jake Ireland <jakewilliami@icloud.com>
2023-12-11 23:09:50 +1300

    Explore parity solution for Part 2 Day 10 2023 in Julia

--- 2023/day10/day10.jl
+++ 2023/day10/day10.jl
@@ -1,20 +1,23 @@
 # Reminds me of day 18 from last year (lava)
+# TODO: Flood fill
+# TODO: to get flood fill working, you will need to increase the resolution:
+# https://www.reddit.com/r/adventofcode/comments/18evyu9/comment/kcqgnoy/
+
+# point_in_path
+# https://github.com/matplotlib/matplotlib/blob/v3.8.2/lib/matplotlib/path.py#L502
+# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L271
+# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L241
+# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L105
+
 
 using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
 
-function parse_input(input_file::String)
-    return readlines_into_char_matrix(input_file)
-    L = readlines(input_file)
-    return L
-end
+parse_input(input_file::String) = readlines_into_char_matrix(input_file)
+
+
+### Part 1 ###
+
+const Index = CartesianIndex{2}
 
 #=
     | is a vertical pipe connecting north and south.
@@ -27,209 +30,70 @@ end
     S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 =#
 
-# const INDEX_LEFT         = CartesianIndex(0, -1)
-# const INDEX_RIGHT        = CartesianIndex(0, 1)
-# const INDEX_ABOVE        = CartesianIndex(1, 0)
-# const INDEX_BELOW        = CartesianIndex(-1, 0)
-# const INDEX_TOP_LEFT     = INDEX_ABOVE + INDEX_LEFT
-# const INDEX_TOP_RIGHT    = INDEX_ABOVE + INDEX_RIGHT
-# const INDEX_BOTTOM_LEFT  = INDEX_BELOW + INDEX_LEFT
-# const INDEX_BOTTOM_RIGHT = INDEX_BELOW + INDEX_RIGHT
+const PIPE_DIRS = Dict{Char, Set{Index}}(
+    '|' => Set{Index}((INDEX_ABOVE, INDEX_BELOW)),
+    '-' => Set{Index}((INDEX_LEFT, INDEX_RIGHT)),
+    'L' => Set{Index}((INDEX_ABOVE, INDEX_RIGHT)),
+    'J' => Set{Index}((INDEX_ABOVE, INDEX_LEFT)),
+    '7' => Set{Index}((INDEX_BELOW, INDEX_LEFT)),
+    'F' => Set{Index}((INDEX_BELOW, INDEX_RIGHT)),
+    '.' => Set{Index}(),
+)
+const DIRS_TO_PIPE = Dict{Set{Index}, Char}(reverse(p) for p in PIPE_DIRS)
 
-function g(t)
-    M = Dict(
-         Set((INDEX_ABOVE, INDEX_BELOW)) => '|',
-         Set((INDEX_LEFT, INDEX_RIGHT)) => '-',
-         Set((INDEX_ABOVE, INDEX_RIGHT)) => 'L',
-         Set((INDEX_ABOVE, INDEX_LEFT)) => 'J',
-         Set((INDEX_BELOW, INDEX_LEFT)) => '7',
-         Set((INDEX_BELOW, INDEX_RIGHT)) => 'F',
-    )
-    return M[t]
-end
+get_pipe_directions(c::Char) = PIPE_DIRS[c]
+get_pipe_from_directions(ds::Set{Index}) = DIRS_TO_PIPE[ds]
 
-function f(c)
-    M = Dict(
-        '|' => Set((INDEX_ABOVE, INDEX_BELOW)),
-        '-' => Set((INDEX_LEFT, INDEX_RIGHT)),
-        'L' => Set((INDEX_ABOVE, INDEX_RIGHT)),
-        'J' => Set((INDEX_ABOVE, INDEX_LEFT)),
-        '7' => Set((INDEX_BELOW, INDEX_LEFT)),
-        'F' => Set((INDEX_BELOW, INDEX_RIGHT)),
-        '.' => Set(),
-        # 'S' => ,
-    )
-    return M[c]
-end
-
-function infer_pipe(i, data)
-    A = []
-    function h!(i, d1, d2, data, A)
-        hasindex(data, i + d1) || return
-        t = f(data[i + d1])
-        d2 in t && push!(A, d2)
-    end
-    h!(i, INDEX_ABOVE, INDEX_BELOW, data, A)
-    h!(i, INDEX_BELOW, INDEX_ABOVE, data, A)
-    h!(i, INDEX_LEFT, INDEX_RIGHT, data, A)
-    h!(i, INDEX_RIGHT, INDEX_LEFT, data, A)
-
-    t = Set(A)
-    return g(t)
-end
-
-function walk!(i, data, path)
-    hasindex(data, i) || return path
-    ds = f(data[i])
-    # println("$(data[i]) ($i) => $ds")
-    for d in ds
+# Infer pipe at starting position based on its surroundings
+function infer_pipe(i::Index, data::Matrix{Char})
+    A = Index[]
+    for d in cardinal_directions(2)
         j = i + d
-        # println("!!! [$i] ", j)
-        if !(j in path)
-            push!(path, j)
-            walk!(j, data, path)
+        hasindex(data, j) || continue
+        t = get_pipe_directions(data[j])
+        d′ = opposite_direction(d)
+        d′ ∈ t && push!(A, d′)
+    end
+
+    t = Set{eltype(A)}(A)
+    return get_pipe_from_directions(t)
+end
+
+# Traverse the pipe system
+function walk_pipe_system(i::Index, data::Matrix{Char}; path::Vector{Index} = Index[])
+    hasindex(data, i) || return path
+    push!(path, i)
+
+    for d in get_pipe_directions(data[i])
+        j = i + d
+        if j ∉ path
+            walk_pipe_system(j, data, path = path)
         end
     end
     return path
 end
 
-function get_cycle(data)
+function get_cycle(data::Matrix{Char})
     data = copy(data)
     si = findfirst(==('S'), data)
     data[si] = infer_pipe(si, data)
-    path = [si]
-    walk!(si, data, path)
+    path = walk_pipe_system(si, data)
     return path
 end
 
-function part1(data)
+function part1(data::Matrix{Char})
     path = get_cycle(data)
     return fld(length(path), 2)
 end
 
-# TODO: Flood fill
-# TODO: to get flood fill working, you will need to increase the resolution:
-# https://www.reddit.com/r/adventofcode/comments/18evyu9/comment/kcqgnoy/
-function is_internal!(
-    i::CartesianIndex{2},
-    points::Vector{CartesianIndex{2}},
-    data,
-    in_points::Set{CartesianIndex{2}} = Set{CartesianIndex{2}}(),
-    out_points::Set{CartesianIndex{2}} = Set{CartesianIndex{2}}(),
-)
-    # If the result has been memoised, return immediately
-    i ∈ in_points && return true
-    i ∈ out_points && return false
 
-    # Otherwise, we need to start looking at each adjacent point
-    seen_points = Set{CartesianIndex{2}}()
-    Q = Queue{CartesianIndex{2}}()
-    enqueue!(Q, i)
-
-    while !isempty(Q)
-        j = dequeue!(Q)
-
-        # Skip this index if:
-        #   - It is part of the structure;
-        #   - We have already processed it; or
-        #   - It's a pipe.
-        j ∈ points && continue
-        j ∈ seen_points && continue
-        hasindex(data, j) || continue
-        data[j] == '.' || continue
-        push!(seen_points, j)
-
-        # TODO: if the current point is part of the cycle, or it is out of bounds
-
-        for d in cardinal_directions(2)
-            enqueue!(Q, j + d)
-        end
-    end
-
-    for p in seen_points
-        push!(in_points, p)
-    end
-
-    return true
-end
-
-
-# https://github.com/matplotlib/matplotlib/blob/v3.8.2/lib/matplotlib/path.py#L502
-# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L271
-# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L241
-# https://github.com/matplotlib/matplotlib/blob/v3.8.2/src/_path.h#L105
-# radius r = 0
-# result[0] != 0
-function points_in_path(points, path)
-    results = zeros(Bool, length(points))
-    length(path) < 3 && return false
-end
-
-#=
-function is_inside_cycle(matrix, cycle_indices, start_index)
-    rows, cols = size(matrix)
-    visited = falses(rows, cols)
-    stack = [(start_index[1], start_index[2])]
-
-    while !isempty(stack)
-        row, col = pop!(stack)
-        visited[row, col] = true
-
-        # Check if the current index is in the cycle
-        if (row, col) in cycle_indices
-            return true
-        end
-
-        # Check neighbors
-        for (dr, dc) in [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            new_row, new_col = row + dr, col + dc
-            if 1 <= new_row <= rows && 1 <= new_col <= cols && !visited[new_row, new_col]
-                push!(stack, (new_row, new_col))
-            end
-        end
-    end
-
-    return false
-end
-
-# Example usage:
-matrix = [
-    1 2 3;
-    4 5 6;
-    7 8 9
-]
-
-cycle_indices = [(1, 1), (1, 2), (2, 2), (2, 1)]  # Example cycle indices
-start_index = (1, 1)  # Example start index
-
-result = is_inside_cycle(matrix, cycle_indices, start_index)
-println(result)
-=#
-function f(si, path, data)
-    visited = falses(size(data))
-    S = Stack{CartesianIndex{2}}()
-    push!(S, si)
-
-    while !isempty(S)
-        i = pop!(S)
-        visited[i] = true
-
-        i ∈ path && return true
-        for j in adjacent_cardinal_indices(data, i)
-            push!(S, j)
-        end
-    end
-
-    return false
-end
-
+### Part 2 ###
 
 function part2(data)
     data = copy(data)
     si = findfirst(==('S'), data)
     data[si] = infer_pipe(si, data)
-    path = [si]
-    walk!(si, data, path)
+    path = walk_pipe_system(si, data)
     # path = get_cycle(data)
 
     VERTICAL_SECTIONS = ("FJ", "L7", "|")
@@ -238,34 +102,94 @@ function part2(data)
 
     # parity solution
     # https://github.com/morgoth1145/advent-of-code/blob/35c7163a89f2b3c6d5e25f4eff3da4c72639284e/2023/10/solution.py#L72
+    # https://github.com/morgoth1145/advent-of-code/blob/b0b7443ec28abbc4e283cb0aa6cf3726eb9199e9/2023/10/solution.py#L83
     # https://www.reddit.com/r/adventofcode/comments/18evyu9/comment/kcqnzmq/
+    # https://www.reddit.com/r/adventofcode/comments/18evyu9/comment/kcqgo61/
+
     #=
-    answer = 0
+    // Section Ends
+    '|' => Set{Index}((INDEX_ABOVE, INDEX_BELOW)),
+    'J' => Set{Index}((INDEX_ABOVE, INDEX_LEFT)),
+    '7' => Set{Index}((INDEX_BELOW, INDEX_LEFT)),
 
-    # Count enclosed tiles by tracking parity per row.
-    # If an odd number of vertical tiles have been seen then any ground tiles
-    # are enclosed, otherwise they are outside. This is similar to checking
-    # if an arbitrary point is enclosed n an arbitrary polygon.
-    for y in range(grid.height):
-        is_enclosed = False
-        current_section = ''
-        for x in range(grid.width):
-            if (x,y) in loop:
-                c = grid[x,y]
-                if c in SECTION_BEGINS:
-                    current_section = c
-                elif c in SECTION_ENDS:
-                    current_section += c
-                    if current_section in VERTICAL_SECTIONS:
-                        is_enclosed = not is_enclosed
-                    current_section = ''
-            else:
-                if is_enclosed:
-                    answer += 1
-
-        assert(not is_enclosed)
+    // Section Begins
+    'F' => Set{Index}((INDEX_BELOW, INDEX_RIGHT)),
+    'L' => Set{Index}((INDEX_ABOVE, INDEX_RIGHT)),
     =#
-    res = 0
+
+    _SECTION_BEGINS = union(get_pipe_directions.(('F', 'L')))
+    _SECTION_ENDS = union(get_pipe_directions.(('J', '7', '|')))
+
+    res, curr_row = 0, 0
+    local is_enclosed::Bool, current_section::String
+
+    for (ri, row) in enumerate(eachrow(data))
+        is_enclosed = false
+        for (ci, c) in enumerate(row)
+            if CartesianIndex(ri, ci) ∈ path
+                c ∈ "|F7" && (is_enclosed = !is_enclosed)
+            else
+                res += is_enclosed
+            end
+        end
+    end
+    return res
+
+    # Odd parity is enclosed
+    # If an odd number of vertical tiles have been seen in any given cell, then any ground tiles are enclosed by pipes.  This is similar to checking if an arbitrary point is enclosed in an arbitrary polygon
+    for (ri, row) in enumerate(eachrow(data))
+        is_enclosed = false
+        current_section = ""
+
+        for (ci, c) in enumerate(row)
+            i = CartesianIndex(ri, ci)
+            if i ∈ path
+                c = data[i]
+                ds = get_pipe_directions(c)
+                if c ∈ SECTION_BEGINS
+                # if all(∈(_SECTION_BEGINS), ds)
+                    current_section = string(c)
+                elseif c ∈ SECTION_ENDS
+                    current_section *= c
+                    # if any(!(iszero ∘ last ∘ Tuple), get_pipe_directions(c))
+                    if current_section ∈ VERTICAL_SECTIONS
+                        is_enclosed = !is_enclosed
+                    end
+                    current_section = ""
+                end
+            else
+                res += is_enclosed
+            end
+        end
+    end
+
+    return res
+
+    for i in CartesianIndicesRowWise(data)
+        this_row = first(i.I)
+        if this_row != curr_row
+            is_enclosed = false
+            current_section = ""
+            curr_row = this_row
+        end
+
+        if i in path
+            c = data[i]
+            if c in SECTION_BEGINS
+                current_section = string(c)
+            elseif c in SECTION_ENDS
+                current_section *= c
+                if current_section in VERTICAL_SECTIONS
+                    is_enclosed = !is_enclosed
+                end
+                current_section = ""
+            end
+        else
+            res += is_enclosed
+        end
+    end
+
+    return res
 
     for y in axes(data, 1)
         is_enclosed = false
@@ -295,7 +219,7 @@ function part2(data)
 
 
     # Memoise internal and external points to reduce computation time
-    in_points, out_points = Set{CartesianIndex{2}}(), Set{CartesianIndex{2}}()
+    in_points, out_points = Set{Index}(), Set{Index}()
 
     # DFS: start at first .
     i = findfirst(==('.'), data)
@@ -332,6 +256,196 @@ function part2(data)
     return res
 end
 
+function part2a(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+    # path = get_cycle(data)
+
+    VERTICAL_SECTIONS = ("FJ", "L7", "|")
+    SECTION_BEGINS = "FL"
+    SECTION_ENDS = "J7|"
+
+    res, curr_row = 0, 0
+    local is_enclosed::Bool, current_section::String
+
+    for y in axes(data, 1)
+        is_enclosed = false
+        current_section = ""
+        for x in axes(data, 2)
+            i = CartesianIndex(y, x)
+            if i in path
+                c = data[i]
+                if c in SECTION_BEGINS
+                    current_section = string(c)
+                elseif c in SECTION_ENDS
+                    current_section *= c
+                    if current_section in VERTICAL_SECTIONS
+                        is_enclosed = !is_enclosed
+                    end
+                    current_section = ""
+                end
+            else
+                res += is_enclosed
+            end
+        end
+    end
+
+    return res
+end
+
+function part2b(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+    # path = get_cycle(data)
+
+    VERTICAL_SECTIONS = ("FJ", "L7", "|")
+    SECTION_BEGINS = "FL"
+    SECTION_ENDS = "J7|"
+
+    res, curr_row = 0, 0
+    local is_enclosed::Bool, current_section::String
+
+    for i in CartesianIndicesRowWise(data)
+        this_row = first(i.I)
+        if this_row != curr_row
+            is_enclosed = false
+            current_section = ""
+            curr_row = this_row
+        end
+
+        if i in path
+            c = data[i]
+            if c in SECTION_BEGINS
+                current_section = string(c)
+            elseif c in SECTION_ENDS
+                current_section *= c
+                if current_section in VERTICAL_SECTIONS
+                    is_enclosed = !is_enclosed
+                end
+                current_section = ""
+            end
+        else
+            res += is_enclosed
+        end
+    end
+
+    return res
+end
+
+function part2c(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+    # path = get_cycle(data)
+
+    VERTICAL_SECTIONS = ("FJ", "L7", "|")
+    SECTION_BEGINS = "FL"
+    SECTION_ENDS = "J7|"
+
+    res, curr_row = 0, 0
+    local is_enclosed::Bool, current_section::String
+
+    for (ri, row) in enumerate(eachrow(data))
+        is_enclosed = false
+        current_section = ""
+
+        for (ci, c) in enumerate(row)
+            i = CartesianIndex(ri, ci)
+            if i in path
+                c = data[i]
+                if c in SECTION_BEGINS
+                    current_section = string(c)
+                elseif c in SECTION_ENDS
+                    current_section *= c
+                    if current_section in VERTICAL_SECTIONS
+                        is_enclosed = !is_enclosed
+                    end
+                    current_section = ""
+                end
+            else
+                res += is_enclosed
+            end
+        end
+    end
+
+    return res
+end
+
+function part2d(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+
+    res = 0
+    local is_enclosed::Bool
+
+    for (ri, row) in enumerate(eachrow(data))
+        is_enclosed = false
+        for (ci, c) in enumerate(row)
+            if CartesianIndex(ri, ci) ∈ path
+                c ∈ "|F7" && (is_enclosed = !is_enclosed)
+            else
+                res += is_enclosed
+            end
+        end
+    end
+
+    return res
+end
+
+function part2e(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+
+    res = 0
+    local is_enclosed::Bool
+
+    for i in CartesianIndicesRowWise(data)
+        isone(last(i.I)) && (is_enclosed = false)
+        if i ∈ path
+            c = data[i]
+            # OR `c ∈ "-FL"`
+            c ∈ "|F7" && (is_enclosed = !is_enclosed)
+        else
+            res += is_enclosed
+        end
+    end
+
+    return res
+end
+
+function part2f(data)
+    data = copy(data)
+    si = findfirst(==('S'), data)
+    data[si] = infer_pipe(si, data)
+    path = walk_pipe_system(si, data)
+
+    res = 0
+    local is_enclosed::Bool
+
+    for i in CartesianIndices(data)
+        isone(first(i.I)) && (is_enclosed = false)
+        if i ∈ path
+            c = data[i]
+            # OR `c ∈ "-FL"`
+            c ∈ "-7J" && (is_enclosed = !is_enclosed)
+
+        else
+            res += is_enclosed
+        end
+    end
+
+    return res
+end
+
 function main()
     data = parse_input("data10.txt")
     # data = parse_input("data10.test.txt")
@@ -345,11 +459,32 @@ function main()
     println("Part 1: $part1_solution")
 
     # Part 2
-    part2_solution = part2(data)
-    @assert part2_solution == 367
+    part2_solution = part2e(data)
     println("Part 2: $part2_solution")
+    @assert part2_solution == 367
     # not 549 too high
     # not 377 too high
 end
 
 main()
+
+
+
+using BenchmarkTools
+
+data = parse_input("data10.txt")
+@btime part2a($data)
+@btime part2b($data)
+@btime part2c($data)
+@btime part2d($data)
+@btime part2e($data)
+@btime part2f($data)
+
+#=
+  155.576 ms (39192 allocations: 1.82 MiB)
+  150.510 ms (39192 allocations: 1.82 MiB)
+  162.644 ms (39192 allocations: 1.82 MiB)
+  148.151 ms (19 allocations: 729.97 KiB)
+  160.134 ms (19 allocations: 729.97 KiB)
+  154.555 ms (19 allocations: 729.97 KiB)
+=#
