cbee83f453db2d083601cf05083eb07d478c160a
Jake Ireland <jakewilliami@icloud.com>
2023-12-13 22:21:38 +1300

    Clean up Julia code for splitting matrices for AoC Day 13 2023
    Final clean up for this day.  Pleased with this answer now.

--- 2023/day13/day13.jl
+++ 2023/day13/day13.jl
@@ -1,4 +1,16 @@
-# I got stuck on part 2 for OVER AN HOUR because the algorithm is greedy :(
+# The puzzle input today is a list of character matrices containing dots and
+# octothorps.  Each of the matrices contain an index at the row or column at
+# which the matrix is reflective.
+#
+# Part 1 asked us to find the reflective index for each matrix.  It was simple,
+# and I took a relatively short amount of time (though the splitting of matrices
+# in the `is_reflective' function was difficult and took some debugging).
+#
+# Part 2 was also straight forward, but I got stuck on it.  That's because my
+# `find_reflection' algorithm was greedy, and I was struggling to find a distinct
+# reflective index after flipping a bit in the matrix.  The thing is, flipping a
+# bit doesn't necessarily invalidate the previous reflective index, so it might
+# still find a distinct one.  That took over an hour to debug...
 
 using AdventOfCode.Parsing, AdventOfCode.Multidimensional
 
@@ -6,6 +18,9 @@ function parse_input(input_file::String)
     L = split(read(input_file, String), "\n\n")
     return Matrix{Char}[Parsing._lines_into_matrix(split(b, "\n")) for b in L]
 end
+
+
+### Part 1 ###
 
 @enum MatrixDimension begin
     invalid = 0
@@ -25,39 +40,35 @@ const invalid_reflective_index = ReflectiveIndex(invalid, 0)
 function is_reflective(M::AbstractArray{<:Any,N}, ri::ReflectiveIndex) where {N}
     i, d = ri.i, Int(ri.dim)
 
-    ai = CartesianIndices(ntuple(k -> k == d ? (max(i - max(min(i, size(M, k) - i), 1) + 1, 1):i) : (firstindex(M, k):lastindex(M, k)), Val{N}()))
-    bi = CartesianIndices(ntuple(k -> k == d ? ((i + 1):min(i + max(min(i, size(M, k) - i), 1), size(M, k))) : (firstindex(M, k):lastindex(M, k)), Val{N}()))
+    # Construct indices to select two views of the matrix split by the reflextive
+    # index along dimension d.  The view of each side must be symmetrical in size.
+    ai = CartesianIndices(ntuple(k -> k == d ? ((i - min(i, size(M, k) - i) + 1):i) : (firstindex(M, k):lastindex(M, k)), Val{N}()))
+    bi = CartesianIndices(ntuple(k -> k == d ? ((i + 1):min(i + min(i, size(M, k) - i), size(M, k))) : (firstindex(M, k):lastindex(M, k)), Val{N}()))
 
-    a = view(M, ai)
-    b = view(M, bi)
-
-    return a == reverse(b, dims=d)
+    return view(M, ai) == reverse(view(M, bi), dims=d)
 end
 
+# Find the reflective index in an m × n matrix by iterating each axis of each dimension.
+# This is a greedy algorithm, so there is also an option to exclude an index from the
+# search, in the interest of part 2 of today's problem.
 function find_reflection(M::Matrix{Char}, not_ri::ReflectiveIndex = invalid_reflective_index)
-    nrows, ncols = size(M)
-
-    for i in axes(M, 1)
-        i == nrows && continue
-        ri = ReflectiveIndex(rows, i)
-        is_reflective(M, ri) && ri != not_ri && return ri
-    end
-
-    for i in axes(M, 2)
-        i == ncols && continue
-        ri = ReflectiveIndex(cols, i)
+    for k in 1:ndims(M), i in axes(M, k)
+        i == size(M, k) && continue
+        ri = ReflectiveIndex(MatrixDimension(k), i)
         is_reflective(M, ri) && ri != not_ri && return ri
     end
 
     return invalid_reflective_index
 end
 
+# Each reflective index is given a score
 function score_reflection(ri::ReflectiveIndex)
     ri.dim == rows && return 100ri.i
     ri.dim == cols && return ri.i
-    return 0
+    error("Unhandled reflective dimension $(ri.dim)")
 end
 
+# Sum scores of each reflective index for part 1
 function part1(data::Vector{Matrix{Char}})
     res = 0
     for M in data
@@ -67,44 +78,35 @@ function part1(data::Vector{Matrix{Char}})
     return res
 end
 
+
+### Part 2 ###
+
+# Flip the character at position `i' of the matrix
 function flip_smudge!(M::Matrix{Char}, i)
-    c = M[i]
-
-    if c == '.'
-        M[i] = '#'
-    elseif c == '#'
-        M[i] = '.'
-    else
-        error("Unhandled char '$c'")
-    end
-
+    M[i] == '.' && (M[i] = '#')
+    M[i] == '#' && (M[i] = '.')
+    M[i] ∈ ".#" || error("Unhandled char '$(M[i])'")
     return M
 end
 
-function new_reflection_valid(new_ri::ReflectiveIndex, old_ri::ReflectiveIndex)
-    new_ri != invalid_reflective_index || return false
-    return new_ri != old_ri
-end
-
+# Find a distinct reflective index after flipping one character
 function find_new_reflection(M::Matrix{Char})
-    ri = find_reflection(M)
-    ri0 = ri
+    ri = ri0 = find_reflection(M)
 
-    for i in CartesianIndices(M)
-        M2 = copy(M)
-        flip_smudge!(M2, i)
-        ri2 = find_reflection(M2, ri0)
-        if new_reflection_valid(ri2, ri0)
-            ri = ri2
+    for i in eachindex(M)
+        M′ = flip_smudge!(copy(M), i)
+        ri1 = find_reflection(M′, ri0)
+        if ri1 ∉ (ri0, invalid_reflective_index)
+            ri = ri1
             break
-        else
-            i == last(CartesianIndices(M)) && println("WARN: no new reflection found")
         end
     end
+
     @assert ri != ri0
     return ri
 end
 
+# Sum scores of alternative reflective indices for part 2
 function part2(data::Vector{Matrix{Char}})
     res = 0
     for M in data
@@ -114,9 +116,11 @@ function part2(data::Vector{Matrix{Char}})
     return res
 end
 
+
+### Main ###
+
 function main()
     data = parse_input("data13.txt")
-    # data = parse_input("data13.test.txt")
 
     # Part 1
     part1_solution = part1(data)
@@ -127,7 +131,6 @@ function main()
     part2_solution = part2(data)
     @assert part2_solution == 28806
     println("Part 2: $part2_solution")
-    # not 37107 too high
 end
 
 main()
