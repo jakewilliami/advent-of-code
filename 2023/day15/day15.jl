bb08427a8c5fb86d3574dab557529896c0df7d0a
Jake Ireland <jakewilliami@icloud.com>
2023-12-15 22:34:45 +1300

    Clean up AoC Day 15 2023 Julia solution

--- 2023/day15/day15.jl
+++ 2023/day15/day15.jl
@@ -1,75 +1,58 @@
-using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
+# We are given a long list of strings separated by commas.  The elves give
+# us a "hashing algorithm" that we have to run on each string and add them
+# together in part 1.
+#
+# Part 2 was Advent of Reading Comprehension.  I had to do this later at
+# night because I had something on, but even still I struggled understanding
+# what turned out to be a simple problem!  The input are actually keys with
+# instructions and we have to place them in a certain way into boxes, and
+# calculate a score at the end of the process.  Similar to part 1, it was
+# very basic, but took a little bit of thinking because English is hard.
+
 using OrderedCollections
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
     S = strip(read(input_file, String))
-    # L = readlines(input_file)
-    L = split(S, ",")
-    # L = get_integers.(L)
-    return L
+    return String.(split(S, ','))
 end
 
-h(c::Char, s=0) = rem((s+Int(c)) * 17, 256)
-function h(s::AbstractString)
+
+### Part 1 ###
+
+function elf_hash(s::AbstractString)
     ans = 0
     for c in s
-        ans = h(c, ans)
+        ans = rem((ans + Int(c)) * 17, 256)
     end
     return ans
 end
 
-function part1(data)
-    for s in data
-        # println(s)
-        # println(h(s))
-    end
-    return sum(h(s) for s in data)
-    res = 0
-    return res
+part1(data::Vector{String}) = sum(elf_hash(s) for s in data)
+
+
+### Part 2 ###
+
+@enum Operation set decrement
+
+struct Instruction
+    op::Operation
+    key::String
+    value::Int
 end
 
-function get_val(s)
+function parse_instruction(s::String)
     if '=' in s
         n, v = split(s, '=')
-        v = parse(Int, v)
-        return n, v
+        return Instruction(set, n, parse(Int, v))
     else
         @assert endswith(s, '-')
-        return s[1:end-1], s
+        return Instruction(decrement, s[1:(end - 1)], 0)
     end
 end
 
-function dis(A)
-    for i in 1:length(A)
-        if !isempty(A[i])
-            println("    Box $(i - 1): $(A[i])")
-        end
-    end
-    println()
-end
-
-function _score_helper(A)
-    D = Dict()
-    for (i, box) in enumerate(A)
-        for (j, (lens, len)) in enumerate(box)
-            D[lens] = (i, j, len)
-        end
-    end
-    return D
-end
-
-function score(A)
-    D = _score_helper(A)
+function calculate_focussing_power(boxes::Vector{OrderedDict{String, Int}})
     res = 0
-    for (i, box) in enumerate(A)
+    for (i, box) in enumerate(boxes)
         for (j, (lens, len)) in enumerate(box)
             res += prod((i, j, len))
         end
@@ -77,70 +60,36 @@ function score(A)
     return res
 end
 
-function part2(data)
-    A = [OrderedDict() for _ in 1:256]
-    # A = Vector(undef, 256)
+function part2(data::Vector{String})
+    boxes = OrderedDict{String, Int}[OrderedDict{String, Int}() for _ in 1:256]
 
     for s in data
-        n, v = get_val(s)
-        m = h(n) + 1
+        instruction = parse_instruction(s)
+        box_i = elf_hash(instruction.key) + 1
 
-        if v isa Int
-            # =
-            if haskey(A[m], n)
-                A[m][n] = v
-            else
-                A[m][n] = v
-            end
+        if instruction.op == set
+            boxes[box_i][instruction.key] = instruction.value
+        elseif instruction.op == decrement
+            pop!(boxes[box_i], instruction.key, nothing)
         else
-            # -
-            if haskey(A[m], n)
-                pop!(A[m], n)
-            end
-        end
-        # println("After \"$s\":")
-        # dis(A)
-    end
-
-    return score(A)
-
-    return A
-
-    return 0
-    D = OrderedDict()
-    # i = 1
-    for s in data
-        n, v = get_val(s)
-        # =
-        if v isa Int
-            if haskey(D, n)
-                D[n] = v
-            else
-                D[n] = v
-            end
-        else # -
-            if haskey(D, n)
-                pop!(D, n)
-            end
+            error("Unhandled operation type $(repr(instruction.op))")
         end
     end
-    println(D)
+
+    return calculate_focussing_power(boxes)
 end
 
 function main()
     data = parse_input("data15.txt")
-    # data = parse_input("data15.test.txt")
-    # println(data)
-    # println(h(data[end]))
 
     # Part 1
     part1_solution = part1(data)
-    # @assert part1_solution ==
+    @assert part1_solution == 515974
     println("Part 1: $part1_solution")
 
     # Part 2
     part2_solution = part2(data)
-    # @assert part2_solution ==
+    @assert part2_solution == 265894
     println("Part 2: $part2_solution")
 end
 
