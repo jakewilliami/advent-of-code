351011ad51091902c918f7f80f418741e31ee713
Jake Ireland <jakewilliami@icloud.com>
2023-12-17 23:30:20 +1300

    Add rough Julia template for any given day AoC day!
    This is the rough format that I start off with for AoC, to make set-up
    easier on any given day.

---
+++ templates/dayxx.jl
@@ -0,0 +1,40 @@
+using AdventOfCode.Parsing, AdventOfCode.Multidimensional
+# using Base.Iterators
+# using Statistics
+# using LinearAlgebra
+# using Combinatorics
+# using DataStructures
+# using StatsBase
+# using IntervalSets
+# using OrderedCollections
+
+function parse_input(input_file::String)
+    # M = readlines_into_char_matrix(input_file)
+    # S = strip(read(input_file, String))
+    L = strip.(readlines(input_file))
+    # L = get_integers.(L)
+    return L
+end
+
+function part1(data)
+end
+
+function part2(data)
+end
+
+function main()
+    data = parse_input("data1.txt")
+    data = parse_input("data1.test.txt")
+
+    # Part 1
+    part1_solution = part1(data)
+    # @assert part1_solution ==
+    println("Part 1: $part1_solution")
+
+    # Part 2
+    part2_solution = part2(data)
+    # @assert part2_solution ==
+    println("Part 2: $part2_solution")
+end
+
+main()
