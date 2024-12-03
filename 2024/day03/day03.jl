8a7651d532445042df7f03db8874f312f82a40b2
Jake Ireland <jakewilliami@icloud.com>
2024-12-03 19:31:36 +1300

    Clean up day 3 solution in Julia

--- 2024/day03/day03.jl
+++ 2024/day03/day03.jl
@@ -1,158 +1,85 @@
-using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+# The input is a string of "corrupted data" that has instructions that
+# we have to find and parse.
+#
+# Part one was very simple; we just had to find the multiplication
+# instructions that looked like `mul(...,...)' (which is very straight
+# forward using regex), apply the multiplication of the arguments, and
+# sum the results.  I did this part in three minutes.
+#
+# For part two, we were to only apply the multiplication if the `mul'
+# command occurs between `do()' and `don't' commands.  I had a solution
+# to this in another few minutes that worked for the test case, but
+# failed for the real data.  I eventually got there, but it too me a
+# long time (and a lot of trial and error: 075f429).  The reason this
+# took me so long was because the do's and don't's don't always alter-
+# nate; there may, for example, be multiple do's before a don't.  And
+# the solutions I was trying to write ended up being over-complicated
+# and failed to account for this nuance.
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    S = strip(read(input_file, String))
-    # L = strip.(readlines(input_file))
-    # L = get_integers.(L)
-    return S
-    return filter(!=('\n'), S)
+    return strip(read(input_file, String))
 end
 
-function part1(data)
-    pat = r"mul\((\d+),(\d+)\)"
-    # data = only(data)
-    c = 0
-    for d in data
-    for m in eachmatch(pat, d)
-        m1 = parse(Int, m[1])
-        m2 = parse(Int, m[2])
-        c += m1*m2
+function part1(data::AbstractString)
+    pat = r"mul\((?<a>\d+),(?<b>\d+)\)"
+
+    return sum(eachmatch(pat, data)) do m
+        a, b = parse.(Int, (m[:a], m[:b]))
+        a * b
     end
-        end
-    return c
 end
 
-function part2(data)
-    pat1 = r"do\(\)"
-    pat2 = r"don't\(\)"
-    pat3 = r"mul\((\d+),(\d+)\)"
-    c = 0
-    d = only([data])
+function part2(data::AbstractString)
+    pat = r"""
+        (?<instruction>do|don't)\(\)|       # [De]activate cursor
+        (?<mul>mul)\((?<a>\d+),(?<b>\d+)\)  # Multiplication command
+    """x
+    enabled = true
 
-    f1 = last.(findall(pat1, d))
-    pushfirst!(f1, 1)
-    f2 = first.(findall(pat2, d))
-        # push!(f2, length(d))
+    return sum(findall(pat, data)) do r
+        # Get RegexMatch object from data view at match
+        m = match(pat, data[r])
+        @assert !isnothing(m)
 
-    for (i, d) in enumerate([data])
-        println("line: $i")
-    f1 = last.(findall(pat1, d))
-    pushfirst!(f1, 1)
-    f2 = first.(findall(pat2, d))
-        push!(f2, length(d))
-        println("$f1, $f2")
+        # Instantiate multiplication components
+        a, b = 0, 0
 
-        # for (s, e) in zip(f1, f2)
-        prev_e = nothing
-        for s in f1
-            if !isnothing(prev_e) && s < prev_e
-                continue
+        # Handle instructions
+        if haskey(m, :instruction) && !isnothing(m[:instruction])
+            # Handle [de]activation of the cursor
+            if m[:instruction] == "do"
+                enabled = true
+            elseif m[:instruction] == "don't"
+                enabled = false
+            else
+                error("Unhandled instruction $(repr(m[:instruction]))")
             end
-            e = f2[findfirst(x -> x > s, f2)]
-            # println("$s $e")
-        d2 = d[s:e]
-        # println(s)
-        # println(e)
-            # println(repr(d2))
-        for m in eachmatch(pat3, d2)
-            m1 = parse(Int, m[1])
-        m2 = parse(Int, m[2])
-        c += m1*m2
-        end
-    end
-        end
-    c = 0
-    for r in findall(pat3, data)
-        s, e = first(r), last(r)
-        # println("$s, $e")
-        if any(s1 < s for s1 in f1) && any(e < e1 for e1 in f2)
-            d2 = data[s:e]
-            for m in eachmatch(pat3, d2)
-            m1 = parse(Int, m[1])
-        m2 = parse(Int, m[2])
-        c += m1*m2
-        end
-        end
-    end
-    c = 0
-    pat = r"(do\(\)|don't\(\))"
-    allowed = true
-    ms = findall(pat, data)
-    pat = r"mul\((\d+),(\d+)\)"
-    used = []
-    i = 1
-    while i <= length(data)-7
-        break
-        if data[i:i+4] == "do()"
-            allowed=true
-        elseif data[i:i+7] == "don't()"
-            allowed=false
-        end
-        if allowed
-            m = eachmatch(pat, data[i:end])
-            if !isnothing(m) && !isempty(m)
-                m = first(m)
-                m1 = parse(Int, m[1])
-        m2 = parse(Int, m[2])
-                c += m1*m2
-                i = last(findfirst(pat, data[i:end]))
+        elseif haskey(m, :mul) && !isnothing(m[:mul])
+            # Set multiplication components if the cursor is enabled
+            @assert m[:mul] == "mul"
+            if enabled
+                a, b = parse.(Int, (m[:a], m[:b]))
             end
+        else
+            error("Unhandled regex match $(repr(m))")
         end
-        i += 1
-    end
-    c = 0
-    D = Dict()
-    allowed = true
-    for i in 1:length(data)-7
-        if data[i:i+3] == "do()"
-            allowed=true
-        elseif data[i:i+6] == "don't()"
 
-            allowed=false
-        end
-        D[i] = allowed
+        a * b
     end
-    # println(D)
-
-    for r in findall(pat, data)
-        # if all(i -> get(D, i, false), r)
-            if get(D, first(r), false)
-            println(r)
-            s = data[r][5:end-1]
-            c += prod(parse.(Int, split(s, ',')))
-        end
-    end
-    c
 end
 
 function main()
     data = parse_input("data03.txt")
-    # data = parse_input("data03.test.txt")
 
     # Part 1
-    # part1_solution = part1(data)
-    # @assert part1_solution ==
-    # println("Part 1: $part1_solution")
+    part1_solution = part1(data)
+    @assert part1_solution == 165225049
+    println("Part 1: $part1_solution")
 
     # Part 2
     part2_solution = part2(data)
-    # @assert part2_solution ==
+    @assert part2_solution == 108830766
     println("Part 2: $part2_solution")
 end
-
-# not 197919940
-# not 492057231
-# not 306080445
-# not 165225049
-# answer was 108830766
 
 main()
