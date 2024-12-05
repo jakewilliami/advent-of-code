6e34547a8c2827f9a30ceea3fcb5dd2b4568ffdc
Jake Ireland <jakewilliami@icloud.com>
2024-12-05 20:33:07 +1300

    Clean up day 5 solution in Julia

--- 2024/day05/day05.jl
+++ 2024/day05/day05.jl
@@ -1,199 +1,143 @@
-using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+# We are given two chunks of data separated by a blank line.  The first data are
+# pairs of numbers, a and b (ordered), which specify a custom ordering: a must come
+# before b.  The second chunk of data is rows of lists of numbers (I naïvely call
+# these data the powerset).
+#
+# Part one requires us to find the rows of the second chunk of data that are in
+# order as per the specified custom ordering instructions (and then count up
+# certain elements in these sets).
+#
+# In part two, we are to find the sets that *aren't* in order, and order then (and
+# then count up certain elements in these newly ordered sets).
+#
+# This day took me a while.  I was actually rather stumped at the problem; I think
+# because you can't just trivially compare two numbers given Instructions and know
+# its order in the list, as you have to compare it to all other numbers in the list.
+# It's quite difficult to explain why I found it difficult.  Nevertheless, it was
+# quite fun once I figured out a solution, though I'm not sure my solution is any
+# good.  This problem reminded me of day 13 of 2022
+
+using DataStructures: DefaultDict
+
+
+### Parse Input ###
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
     S = strip(read(input_file, String))
     S1, S2 = split(S, "\n\n")
-    L1 = [parse.(Int, split(l, '|')) for l in split(S1, '\n') if !isempty(strip(l))]
-    L2 = [parse.(Int, split(l, ',')) for l in split(S2, '\n') if !isempty(strip(l))]
-    # L = strip.(readlines(input_file))
-    # L = get_integers.(L)
-    return L1, L2
+
+    # Get instructions as a list of tuples
+    I = Tuple{Int, Int}[Tuple(parse.(Int, split(l, '|'))) for l in split(S1, '\n')]
+
+    # Get a powerset 𝒫 (\scrP); a list of vectors with order and possible duplicates
+    𝒫 = Vector{Int}[parse.(Int, split(l, ',')) for l in split(S2, '\n')]
+
+    return Instructions(I), 𝒫
 end
 
-function allowed_before(a::Int, b::Int, data)
-    # x must come before y
-    v1,v2 = [], []
-    for (x, y) in data
-        if x == a && y == b
-            return true
+struct Instructions
+    # Values indicate all numbers that should come strictly after the key
+    data::DefaultDict{Int, Vector{Int}}
+
+    function Instructions(data::Vector{Tuple{Int, Int}})
+        D = DefaultDict{Int, Vector{Int}}(Vector{Int})
+
+        for (a, b) in data
+            push!(D[a], b)
         end
-        if x == b && y == a
-            return false
-        end
-    end
 
-    return true
-end
-
-function check(l::Vector{Int}, data)
-    for i in 1:length(l)
-        if !all(allowed_before(l[i], l[j], data) for j in (i+1):length(l))
-            return false
-        end
-    end
-    return true
-end
-
-function part1(data)
-    L1, L2 = data
-
-    r = 0
-    for l in L2
-        if check(l, L1)
-            i = fld(length(l), 2) + 1
-            r += l[i]
-        end
-    end
-
-    r
-end
-
-using Combinatorics
-
-function find_before(a, data, used, order)
-    for (x, y) in data
-        if y == a && y ∉ used
-            return x
-        end
+        new(D)
     end
 end
 
-L1, L2 =  parse_input("data05.txt")
-# L1, L2 =  parse_input("data05.test.txt")
 
-function correct_order(a, b, data)
-    # an = true
-    for (x, y) in data
-        if x == a && y == b
-            return true
-        end
-        # if x ever comes before y
-        if x == b && y == a
-            return false
-        end
+### Part 1 ###
+
+# As per the provided instructions, is a allowed to come before b?
+allowed_before(a::Int, b::Int, I::Instructions) =
+    b ∈ I.data[a]
+
+# As per the provided instructions, is a allowed to come before all elements of b?
+function allowed_before(a::Int, b::Vector{Int}, I::Instructions)
+    return all(1:length(b)) do i
+        allowed_before(a, b[i], I)
     end
-
-    return true
 end
 
-struct MO <: Base.Order.Ordering end
-Base.Order.lt(_o::MO, a, b) = correct_order(a, b, L2)
-# Base.Order.lt(_o::MO, a, b) = correct_order(a, b, L2)
-# Base.Order.gt(_o::MO, b, a) = allowed_before(b, a, L2)
-
-
-struct F <: AbstractVector{Int}
-    data::Vector{Int}
+# Check that the given set is correctly ordered as per the provided instructions
+function allowed_order(S::Vector{Int}, I::Instructions)
+    # The idea is to look through each element of the set and ensure that all
+    # elements that succeed it are allowed to do so
+    return all(1:length(S)) do i
+        allowed_before(S[i], S[i + 1:end], I)
+    end
 end
 
-function findfirst_allowed(l, data)
-    for i in 1:length(l)
-        l2 = deepcopy(l)
-        x = l2[i]
-        deleteat!(l2, i)
-        if all(allowed_before(x, y, data) for y in l2)
-            return (i, x)
+function part1(I::Instructions, 𝒫::Vector{Vector{Int}})
+    return sum(𝒫) do S
+        allowed_order(S, I) || return 0
+        S[length(S) ÷ 2 + 1]
+    end
+end
+
+
+### Part 2 ###
+
+# Given a set, find the element that should go before all others as per provided
+# instructions.  Optionally specify an offset from which to start checking
+function findfirst_allowed(S::Vector{Int}, I::Instructions; offset::Int = 0)
+    offset == length(S) && return last(S)
+    S′ = @view S[(firstindex(S) + offset):lastindex(S)]
+    for i in eachindex(S′)
+        if all(allowed_before(S′[i], S′[j], I) for j in setdiff(eachindex(S′), i))
+            return (i + offset, S′[i])
         end
     end
 end
 
-function find_lowest(data)
-    i = 1
-    order = []
-    l, r = data[i]
-    res = find_before(l, data, Set(), order)
-    res_prev = res
-    used = Set((l, res))
-    pushfirst!(order, l)
-    pushfirst!(order, res)
-    while !isnothing(res)
-        res_prev = res
-        res = find_before(res, data, used, order)
-        if !isnothing(res)
-            push!(used, res)
-            pushfirst!(order, res)
-        end
+# Find an allowed order for the given set as per provided instructions
+function correct_order!(S::Vector{Int}, I::Instructions)
+    # The idea is incrementally move an element of a slice of the set near
+    # the front of
+    changes = 0
+    while changes < length(S)
+        i, x = findfirst_allowed(S, I, offset = changes)
+        deleteat!(S, i)
+
+        # Why does this work?  Since optimising/cleaning this solution this
+        # code doesn't look like it should work anymore...but it does?
+        #
+        # Old code:
+        # <https://github.com/jakewilliami/advent-of-code/blob/97420b0/2024/day05/day05.jl#L130-L143>
+        pushfirst!(S, x)
+        changes += 1
     end
-    println(order)
-    return res_prev
+    return S
 end
 
-function correct_order(l, data)
-    l2 = []
-
-    l′ = deepcopy(l)
-    while true
-        if !isempty(l′)
-            i, x = findfirst_allowed(l′, data)
-            push!(l2, x)
-            deleteat!(l′, i)
-        else
-            break
-        end
+function part2(I::Instructions, 𝒫::Vector{Vector{Int}})
+    return sum(𝒫) do S
+        allowed_order(S, I) && return 0
+        correct_order!(S, I)
+        S[length(S) ÷ 2 + 1]
     end
-    return l2
-
-    #=for p in permutations(l)
-        # println(p, ": ", check(l, data))
-        if check(p, data)
-            return p
-        end
-    end=#
-    # println(find_lowest(data))
-    println(sort(l, order=MO()))
-    l
 end
 
-function part2(data)
-    L1, L2 = data
-    # println(check([97, 75, 47, 61, 53], L1))
-    # println(check([61,29,13], L1))
-    # println(check([97,75,47,29,13], L1))
 
-    r = 0
-    for l in L2
-        if !check(l, L1)
-            # println("-==================")
-            # println(l)
-            # println(findfirst_allowed(l, data))
-
-            # l′ = sort(l, order=MO())
-            l′ = correct_order(l, L1)
-            println(l, " => ", l′)
-            i = fld(length(l), 2) + 1
-            r += l′[i]
-        end
-    end
-
-    r
-end
-
+### Main ###
 
 function main()
-    data = parse_input("data05.txt")
-    # data = parse_input("data05.test.txt")
+    I, 𝒫 = parse_input("data05.txt")
 
     # Part 1
-    part1_solution = part1(data)
-    # @assert part1_solution ==
+    part1_solution = part1(I, 𝒫)
+    @assert part1_solution == 5509
     println("Part 1: $part1_solution")
 
     # Part 2
-    part2_solution = part2(data)
-    # @assert part2_solution ==
+    part2_solution = part2(I, 𝒫)
+    @assert part2_solution == 4407
     println("Part 2: $part2_solution")
 end
 
 main()
-
-
-# not 5198
