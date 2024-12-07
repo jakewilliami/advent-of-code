0a0daeb33f0c010244b0cb2c268b91b49553886b
Jake Ireland <jakewilliami@icloud.com>
2024-12-07 23:51:59 +1300

    Clean up day 7 solution in Julia

--- 2024/day07/day07.jl
+++ 2024/day07/day07.jl
@@ -1,98 +1,116 @@
+# Each line of the input contained an expected value on the left, and
+# a list of numbers on the right.  We had to find if some combination
+# of selected operators applied from left to right to the components
+# would make the left hand value.
+#
+# In part one, the allowed operators were plus and multiplication.  In
+# part two, we were also allowed to concatonate numbers together as if
+# they were strings.
+#
+# I did okay today, but I wasn't particularly fast.  I had no real bugs
+# in the program because I did it in the simplest way I could think to
+# do it: brute force.  The only problem I had was remembering how to
+# generate all possible combinations of operators.  I was trying solutions
+# with Combinatorics.jl but forgot that this is simple and can be done
+# using Base.Iterators.product.  I was also doing this at a party
+# (a family friend's 60th) and was talking through my thinking to an
+# interested party-goer.
+#
+# I couldn't think of one, but similar to yesterday, I do wonder if there
+# is a better, non-brute-force solution to this.  (And I am once again
+# greatful to be using Julia for its speed.)
+
 using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+
+### Parse Input ###
+
+struct UnsetEquation
+    expected::Int
+    components::Vector{Int}
+end
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    # S = strip(read(input_file, String))
     L = strip.(readlines(input_file))
-    L = [strip.(split(l, ':')) for l in L]
-    L1 = [parse(Int, l) for (l, _) in L]
-    L2 = [parse.(Int, split(l)) for (_, l) in L]
-    # L = get_integers.(L)
-    return L1, L2
-end
-
-function ops(v)
-    return collect(Base.Iterators.product(((+, *) for i in 1:(length(v)-1))...))
-    [p for p in permutations((+, *), length(v) - 1)]
-end
-
-function apply_op(v, ov)
-    r = ov[1](v[1], v[2])
-    for i in 2:length(ov)
-        r = ov[i](r, v[i + 1])
+    A = UnsetEquation[]
+    for line in L
+        a, b = strip.(split(line, ':'))
+        a, b = parse(Int, a), parse.(Int, split(b))
+        push!(A, UnsetEquation(a, b))
     end
-    r
+    return A
 end
 
-function part1(data)
-    L1, L2 = data
-    r = 0
-    for (n, (l1, l2)) in enumerate(zip(L1, L2))
-        a = any(apply_op(l2, o) == l1 for o in ops(l2))
-        if a
-            r += sum(l1)
-        end
-    end
-    r
+
+### Part 1 ###
+
+function op_combinations(e::UnsetEquation, ops::NTuple{N, Symbol}) where {N}
+    n = length(e.components) - 1
+    return Base.Iterators.product((ops for i in 1:n)...)
 end
 
-function ops2(v)
-    return collect(Base.Iterators.product((('+', '*', '|') for i in 1:(length(v)-1))...))
-end
-
-function apply_op1(op, v1, v2)
-    if op == '+'
-        return v1 + v2
-    elseif op == '*'
-        return v1 * v2
-    elseif op == '|'
-        return parse(Int, join((v1, v2)))
+function get_op(op_s::Symbol)
+    op = identity
+    if op_s == :+
+        op = +
+    elseif op_s == :*
+        op = *
+    elseif op_s == :||
+        # Additional handling of new operator for part 2
+        # The naïve string joining for concat op is much slower
+        # op = (a, b) -> parse(Int, join((a, b)))
+        op = (a, b) -> a * 10^ndigits(b) + b
     else
-        error(op)
+        error("Unhandled operator: $op_s")
+    end
+    return op
+end
+
+function apply_op(e::UnsetEquation, ops::NTuple{N, Symbol}) where {N}
+    @assert length(ops) > 1
+    r = get_op(ops[1])(e.components[1], e.components[2])
+    for i in 2:length(ops)
+        op_s = ops[i]
+        op = get_op(ops[i])
+        @assert op != identity
+        r = op(r, e.components[i + 1])
+    end
+    return r
+end
+
+function applied_expected(e::UnsetEquation, ops::NTuple{N, Symbol}) where {N}
+    @assert length(ops) == length(e.components) - 1
+    return apply_op(e, ops) == e.expected
+end
+
+function compute_ans(data::Vector{UnsetEquation}; ops::NTuple{N, Symbol}) where {N}
+    sum(data) do e
+        any(applied_expected(e, op) for op in op_combinations(e, ops)) ||
+            return 0
+        e.expected
     end
 end
 
-function apply_op2(v, ov)
-    r = apply_op1(ov[1], v[1], v[2])
-    for i in 2:length(ov)
-        r = apply_op1(ov[i], r, v[i + 1])
-    end
-    r
-end
+part1(data::Vector{UnsetEquation}) = compute_ans(data, ops = (:+, :*))
 
-function part2(data)
-    L1, L2 = data
-    r = 0
-    for (n, (l1, l2)) in enumerate(zip(L1, L2))
-        println("$n/$(length(L1))")
-        a = any(apply_op2(l2, o) == l1 for o in ops2(l2))
-        if a
-            r += sum(l1)
-        end
-    end
-    r
-end
+
+### Part 2 ###
+
+part2(data::Vector{UnsetEquation}) = compute_ans(data, ops = (:+, :*, :||))
+
+
+### Main ###
 
 function main()
     data = parse_input("data07.txt")
-    # data = parse_input("data07.test.txt")
 
     # Part 1
-    # part1_solution = part1(data)
-    # @assert part1_solution ==
-    # println("Part 1: $part1_solution")
+    part1_solution = part1(data)
+    @assert part1_solution == 5030892084481
+    println("Part 1: $part1_solution")
 
     # Part 2
     part2_solution = part2(data)
-    # @assert part2_solution ==
+    @assert part2_solution == 91377448644679
     println("Part 2: $part2_solution")
 end
 
