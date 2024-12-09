6a73f95f11f76ba8724165e2981d45eaa651338a
Jake Ireland <jakewilliami@icloud.com>
2024-12-09 20:39:47 +1300

    Clean up day 9 solution in Julia

--- 2024/day09/day09.jl
+++ 2024/day09/day09.jl
@@ -1,228 +1,171 @@
-using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+# We were given a long line as an input, containing digits.  Each digits
+# represented disp space in alternating format:
+#   The disk map uses a dense format to represent the layout of files and
+#   free space on the disk.  The digits alternate between indicating the
+#   length of a file and the length of free space.
+#
+# In part one, we had to move files from the right to the leftmost empty
+# space, and then calculate some index-based checksum of the filesystem.
+# In part two, we had to do the same, but could only move files in the
+# blocks that they were originally in.
+#
+# For both parts, I actually simulated this with a vector rather than
+# keeping track of positions and size (not immediately expanding the
+# spaces) and doing index and range maths based on that.  It is slow to
+# run, of course.  I was a bit slow to implement this for some reason;
+# I did okay but not well.
 
 
+### Parse Input ###
+
+const FileOrFree = Union{Int, Nothing}
+const FileSystem = Vector{FileOrFree}
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    # S = strip(read(input_file, String))
-    L = strip.(readlines(input_file))
-    L = only(L)
-    L = [parse(Int, c) for c in L]
-    # L = get_integers.(L)
-    return L
+    L = strip(only(readlines(input_file)))
+    return FileOrFree[parse(Int, c) for c in L]
 end
 
-function expand(data)
-    A = []
-    j = 0
-    for (i, x) in enumerate(data)
-        if isodd(i)
-            append!(A, fill(j, x))
-            j += 1
-        else
-            append!(A, fill(nothing, x))
-        end
-    end
-    A
-end
 
-function findfirst_empty(A)
-    findfirst(isnothing, A)
-end
+### Part 1 ###
 
-function repr1(A)
-    io = IOBuffer()
-    for i in A
-        if isnothing(i)
-            print(io, '.')
-        else
-            print(io, i)
-        end
-    end
-    String(take!(io))
-end
+# Complicated but memory-efficient process of expanding the diskmap representation
+function expand!(fs::FileSystem)
+    # Step 1: collate file and free space information
+    files = Int[fs[i] for i in 1:2:length(fs)]
+    free_space = Int[fs[i] for i in 2:2:length(fs)]
 
-function has_free_space(A)
-    any(isnothing, A)
-end
-
-function trim_empty!(A)
-    while !isempty(A) && isnothing(last(A))
-        pop!(A)
-    end
-end
-
-function left_align!(A)
-    A′ = deepcopy(A)
-    # A′ = Union{Int, Nothing}[nothing for _ in 1:length(A)]
-    while has_free_space(A′)
-        x = pop!(A′)
-        isnothing(x) && continue
-        i = findfirst_empty(A′)
-        if !isnothing(i)
-            # println("i=$i, x=$x")
-            A′[i] = x
-        else
-            push!(A′, x)
-        end
-        # println(repr1(A′))
+    # Step 2: calculate offsets to be used later
+    offsets = Dict{Int, Int}(0 => 0)
+    for (i, (a, b)) in enumerate(zip(cumsum(files), cumsum(free_space)))
+        offsets[i] = a + b - i
     end
 
-    trim_empty!(A′)
-    A′
-end
+    # Step 3: remove free space information from disk map
+    deleteat!(fs, 2:2:length(fs))
 
-function checksum(A)
-    r = 0
-    for (i, x) in enumerate(A)
-        isnothing(x) && continue
-        j = i - 1
-        r += j*x
+    # Step 4: add free space
+    for (i, (p, n)) in enumerate(zip(free_space, cumsum(free_space)))
+        offset = n - p
+        j = i + offset + 1
+        splice!(fs, j:(j - 1), (nothing for _ in 1:p))
     end
-    r
+
+    # Step 5: expand files
+    for (i, n) in enumerate(files)
+        id = i - 1
+        j = i + offsets[id]
+        fs[j] = id
+        splice!(fs, j:(j - 1), (id for _ in 1:(n - 1)))
+    end
+
+    return fs
 end
 
-function part1(data)
-    A = expand(data)
-    A = left_align!(A)
-    checksum(A)
+# Free space in the FileSystem is represented as nothing
+isfree(x::FileOrFree) = isnothing(x)
+
+function rstrip!(fs::FileSystem)
+    i = findlast(!isfree, fs)
+    if !isnothing(i)
+        deleteat!(fs, (i + 1):lastindex(fs))
+    end
+    return fs
 end
 
-function find_rightmost_start(A, start_id)
-    for i in length(A):-1:1
-        A[i] ≤ start_id && return i
+function left_align!(fs::FileSystem)
+    while any(isfree, fs)
+        rstrip!(fs)
+        x = pop!(fs)
+        i = findfirst(isfree, fs)
+        @assert !isnothing(i)
+        fs[i] = x
+    end
+
+    return rstrip!(fs)
+end
+
+function checksum(fs::FileSystem)
+    sum(enumerate(fs)) do (i, x)
+        isfree(x) && return 0
+        x * (i - 1)
     end
 end
 
-function findlast_nonempty(A, start_id)
-    for i in find_rightmost_start(A, start_id):-1:1
-        x = A[i]
-        @assert x ≤ start_id
-        if !isnothing(x)
-            return i
-        end
-    end
-    # findlast(!isnothing, A)
+function part1(fs::FileSystem)
+    expand!(fs)
+    left_align!(fs)
+    return checksum(fs)
 end
 
-function findnext_rightmost_nonempty(A, i, start_id)
-    j = find_rightmost_start(A, start_id)
-    x = A[j]
 
-    while 1 ≤ j ≤ length(A) && !isnothing(A[j]) && A[j] == x && A[j] ≤ start_id
-        j -= 1
-    end
+### Part 2 ###
 
-    iszero(j) && return nothing
-    return j + 1
-end
-
-function find_rightmost_chunk_span(A, start_id)
-    i = findlast_nonempty(A, start_id)
-    j = findnext_rightmost_nonempty(A, i, start_id)
-    j, i
-end
-
-function find_leftmost_viable_chunk_index(A, n)
-    for i in 1:(length(A)-n)
-        if all(isnothing, A[i:(i+n-1)])
-            return i
-        end
+function findfirst_viable_block(fs::FileSystem, block_size::Int)
+    for i in 1:(length(fs) - block_size)
+        all(isnothing, fs[i:(i + block_size - 1)]) && return i
     end
 end
 
-function last_nonempty(A)
-    for i in length(A):-1:1
-        if !isnothing(A[i])
-            return A[i]
-        end
+function last_nonempty(fs::FileSystem)
+    i = findlast(!isfree, fs)
+    if !isnothing(i)
+        return fs[i]
     end
 end
 
-function has_viable_space(A, n)
-    for i in 1:(length(A)-n)
-        if all(isnothing, A[i:(i+n-1)])
-            return true
-        end
-    end
-    return false
+function find_block(fs::FileSystem, id::Int)
+    indices = findall(==(id), fs)
+    @assert all(indices[i - 1] == indices[i] - 1 for i in 2:length(indices))
+    isempty(indices) && return 0:-1
+    return UnitRange{Int}(extrema(indices)...)
 end
 
-function left_align_in_chunks!(A)
-    A′ = deepcopy(A)
-    # println(repr1(A))
-    #
-    # println(i,", ", j)
-    # println(join(A[i:j]))
+function left_align_in_blocks!(fs::FileSystem)
+    for id in last_nonempty(fs):-1:0
+        file_block = find_block(fs, id)
+        n = length(file_block)
 
-    for id in last_nonempty(A′):-1:0
-        # splice!(A, 4:3, 2)
-        indices = findall(==(id), A′)
-        @assert all(indices[i - 1] == indices[i] - 1 for i in 2:length(indices))
-        n = length(indices)
-        if has_viable_space(A′, n)
-            k = find_leftmost_viable_chunk_index(A′, n)
-            any(k > ii for ii in indices) && continue  # edge case
-            @assert !isnothing(k)
-            x = []
-            for i in indices
-                push!(x, A′[i])
-                A′[i] = nothing
-            end
-            # x = deleteat!(A′, indices)
-            # println(x)
-            splice!(A′, k:(k+n - 1), x)
-            # println(repr1(A′))
+        # Find a chunk of free space that would fit the current block
+        k = findfirst_viable_block(fs, n)
+        if !isnothing(k)
+            # We shouldn't move anything further to the right
+            # than it presently is
+            k > last(file_block) && continue
+
+            # copyto!(dest, do, src, so, N)
+            # Copy N elements from collection src starting at the linear
+            # index `so`, to array dest starting at the index `do`
+            copyto!(fs, k, fs, first(file_block), n)
+
+            # Free the space we moved the block from
+            splice!(fs, file_block, (nothing for _ in file_block))
         end
     end
 
-    return A′
-
-
-    while has_free_space(A′)
-
-        id -= 1
-        n = j - i + 1
-        trim_empty!(A′)
-        k = find_leftmost_viable_chunk_index(A′, n)
-        isnothing(k) && continue
-        for m in 1:n
-            x = pop!(A′)
-            A′[k + m - 1] = x
-        end
-        println(repr1(A′))
-    end
-
-    trim_empty!(A′)
-    A′
+    return fs
 end
 
-function part2(data)
-    A = expand(data)
-    A = left_align_in_chunks!(A)
-    checksum(A)
+function part2(fs::FileSystem)
+    expand!(fs)
+    left_align_in_blocks!(fs)
+    checksum(fs)
 end
+
+
+### Main ###
 
 function main()
     data = parse_input("data09.txt")
-    # data = parse_input("data09.test.txt")
 
     # Part 1
-    part1_solution = part1(data)
-    # @assert part1_solution ==
+    part1_solution = part1(deepcopy(data))
+    @assert part1_solution == 6463499258318
     println("Part 1: $part1_solution")
 
     # Part 2
-    part2_solution = part2(data)
-    # @assert part2_solution ==
+    part2_solution = part2(deepcopy(data))
+    @assert part2_solution == 6493634986625
     println("Part 2: $part2_solution")
 end
 
