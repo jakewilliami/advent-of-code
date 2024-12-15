5253012c00f51f38fb9c662317b5be1359cebd63
Jake Ireland <jakewilliami@icloud.com>
2024-12-15 23:50:29 +1300

    Clean up day 15 solution in Julia

--- 2024/day15/day15.jl
+++ 2024/day15/day15.jl
@@ -1,47 +1,64 @@
+# There a two parts to the input: a grid, and some instructions.  In the grid,
+# there is a robot (represented by the @ character), some boxes (represented by
+# O), some walls (represented by #), and free space (represented by .).  The
+# robot can move in each four cardinal directions as per its instructions, and
+# it can move any number of boxes, but it cannot move boxes or itself into any
+# walls.
+#
+# Tricky problem today.  In theory/on paper it's simple, but in practice, any
+# implementation is rather hard to get right.  Part one asked us to just simulate
+# its movements as per the instructions and calculate the position of each box
+# at the end of the simulation.  I did this very slowly because I had so many
+# implementation issues.  I even had a bug that was very rare to occur; in the
+# larger test example, this was only encountered after 380 steps.
+#
+# In part two, everything was wider, which mean that boxes take up two squares
+# and can potentially move two boxes.  I was reading through the solutions thread
+# for some help and it was clear that a recursive solution was best for this:
+#   <https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24swhr/>
+#
+# I ended up writing a solution to part two based on this other solution:
+#   <https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24z445/>
+#
+# I understand how it works, but I think it would have taken me a long time to
+# get right without the guidance...
+
 using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+
+
+### Parse Input ###
 
 const Index = CartesianIndex{2}
-
-function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    S = strip(read(input_file, String))
-    # L = strip.(readlines(input_file))
-    S1, S2 = split(S, "\n\n")
-    M = Parsing._lines_into_matrix(split(strip(S1), '\n'))
-    S2 = strip.(split(S2, '\n'))
-    return M, join(S2)
-    # L = get_integers.(L)
-    return L
-end
+const DIRECTIONS = Dict{Char, Direction}(
+    '>' => INDEX_RIGHT,
+    '<' => INDEX_LEFT,
+    'v' => INDEX_DOWN,
+    '^' => INDEX_UP,
+)
 
 function dir(c::Char)
-    if c == '>'
-        return INDEX_RIGHT
-    elseif c == '<'
-        return INDEX_LEFT
-    elseif c == 'v'
-        return INDEX_DOWN
-    elseif c == '^'
-        return INDEX_UP
-    else
-        error("unhandled '$c'")
-    end
+    haskey(DIRECTIONS, c) || error("Unhandled direction character '$c'")
+    return DIRECTIONS[c]
 end
 
-isrobot(c) = c == '@'
-isbox(c) = c == 'O'
-iswall(c) = c == '#'
-isfree(c) = c == '.'
+function parse_input(input_file::String)
+    S = strip(read(input_file, String))
+    S1, S2 = split(S, "\n\n")
+    M = Parsing._lines_into_matrix(split(strip(S1), '\n'))
+    I = join(strip.(split(S2, '\n')))
+    L = Direction[dir(c) for c in I]
+    return M, L
+end
 
-function robot_pos(M)
+
+### Part 1 ###
+
+isrobot(c::Char) = c == '@'
+isbox(c::Char) = c ∈ "O[]"
+iswall(c::Char) = c == '#'
+isfree(c::Char) = c == '.'
+
+function find_robot(M::Matrix{Char})
     for i in CartesianIndices(M)
         if isrobot(M[i])
             return i
@@ -49,193 +66,46 @@ function robot_pos(M)
     end
 end
 
-function next_wall(M, i, d)
-    hasindex(M, i) && iswall(M[i]) && return i
-    j = i + d
-    while hasindex(M, j) && !iswall(M[j])
-        j += d
-    end
-    return j
-end
-
-function can_move(M, i, d)
-    hasindex(M, i) && iswall(M[i]) && return false
-    j = i + d
-    # println("here: j=$j, $(M[j])")
-    if hasindex(M, j) && isfree(M[j])
-        return true
-    end
-    j += d
-    hasindex(M, j) || return false
-    while true
-        # while hasindex(M, j) && (isfree(M[j]) || isbox(M[j])) && !iswall(M[j])
-        # println("here: j=$j, $(M[j]), $(isfree(M[j]))")
-        if !isbox(M[j])#iswall(M[j]) || isfree(M[j])
-            break
-        end
-        j += d
-    end
-    # println("here: j=$j, $(M[j-d]), $(isfree(M[j-d]))")
-    return isfree(M[j])
-end
-
-function next_move_stop(M, i, d)
-    hasindex(M, i) && iswall(M[i]) && return i
-    j = i + d
-    while hasindex(M, j) && (isfree(M[j]) || isbox(M[j])) && !iswall(M[j])
-        j += d
-    end
-    if !isfree(M[j - d])
-        return i
-    end
-    return j - d
-end
-
-# returns new robot position
-function move!(M, i, inst::Char)
-    i = robot_pos(M)
-    initial_i = i
-    d = dir(inst)
+# Returns new robot position
+function move!(M::Matrix{Char}, i::Index, d::Direction)
     c = M[i]
-    @assert isrobot(c)
-
-    can_move(M, i, d) || return i
-
     j = i + d
+    @assert isrobot(c) "M[i]=M[$i]=$(M[i])≠'@'"
 
-    # SOMEHOW AN EDGE CASE THAT WAS NOT PICKED UP BY CAN MOVE, BUT ONLY ENCOUNTERED AFTER 380 STEPS IN THE TEST CASE
+    # Case 1: next position is free space
+    if isfree(M[j])
+        M[j] = M[i]
+        M[i] = '.'
+        return j
+    end
+
+    # Case 2: next position is a wall so we can't move
     if iswall(M[j])
         return i
     end
-    # if isfree(M[j])
-        # M[i] = '.'
-        # M[j] = '@'
-    # end
 
-    # println("here")
-
+    # Case 3: next position is a box that we might be able to move
+    @assert isbox(M[j])
     k = j
     while isbox(M[k])
         k += d
     end
+    @assert !isbox(M[k])
 
-    while true
-        # println(k)
-        c = M[k - d]
-        # M[k] = iswall(c) ? '.' : c
-        # Account for edge case where robot is moving but surrounded by boxes
-        c = '.'
-        if isrobot(M[k - d])
-            c = '@'
-        elseif isbox(M[k - d])
-            # then either we are pushing a row of boxes or the robot has a box behind it
-            if !isrobot(M[k])
-                # c = '.'
-            # else
-                c = 'O'
-            end
-        end
-        M[k] = c
-        k == i && break
-        k -= d
-    end
-    return k + d
-
-    # k -= d
-    k′ = k - d
-    println(i,k, k′)
-    while k′ != i
-        println("M[k′]=M[$k′]='$(M[k′])', M[k]=M[$k]='$(M[k])'")
-        M[k] = M[k′]
-        k = k′
-        k′ -= d
-    end
-    # if k != i
-        # M[k] = '.'
-    # end
-    # return i + d
-    return k′
-
-
-
-
-
-
-    wi = next_wall(M, i, d)
-
-    # if isfree(M[i])
-        # M[i] = '.'
-        # M[j] = '@'
-        # return j
-    # end
-
-    old_c = c
-    next_i = next_move_stop(M, i, d)
-    println(next_i)
-    # println(i, d, next_i)
-    # println(next_i)
-    while j != next_i
-    # while can_move(M, j, d)
-        println("j=$j")
-        M[i] = old_c
-        new_c = M[j]
-        M[j] = c
-        old_c = c
-        c = new_c
-        i = j
-        j += d
-    end
-    M[j] = c
-    M[initial_i] = '.'
-    return initial_i + d
-
-    while j != wi
-        M[i] = '.'
-        new_c = M[j]
-        M[j] = c
-        c = new_c
-        isfree(c) && return j
-        i = j
-        j += d
-    end
-    return j - d
-
-    hasindex(M, j) || return i
-    iswall(M[j]) && return i
-
-    if isfree(M[j])
-        M[i] = '.'
-        M[j] = '@'
-        return j
+    # We have reached the end of a row of boxes.  Can we move them?
+    # Case 3.1: No, there's a wall in the way
+    if iswall(M[k])
+        return i
     end
 
-    n = 0
-    while hasindex(M, j) && isbox(M[j])
-        # move from i to j
-        M[i] = '.'
-        new_c = M[j]
-        M[j] = c
-        c = new_c
-        i = j
-        j += d
-        n += 1
-    end
-    M[j] = c
-    M[j - n*d] = '@'
-    return j - d
-end
-
-function displ(M)
-    io = IOBuffer()
-    for row in eachrow(M)
-        for c in row
-            print(io, c)
-        end
-        println(io)
-    end
-    s = String(take!(io))
-    println(s)
-    return s
+    # Case 3.2: Yes, there's free space.  We can use a trick where we simply
+    # move the first box to the end:
+    # <https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24swhr/>
+    @assert isfree(M[k])
+    M[k] = M[j]
+    M[j] = M[i]
+    M[i] = '.'
+    return j
 end
 
 function gps(i::Index)
@@ -243,55 +113,34 @@ function gps(i::Index)
     100*(y - 1) + (x - 1)
 end
 
-function answer(M)
-    sum(CartesianIndices(M)) do i
+function part1(M, I)
+    # Simulate robot movements as per instructions
+    i = find_robot(M)
+    for d in I
+        i = move!(M, i, d)
+    end
+
+    # Sum together gps coordinates of each box after simulation
+    return sum(CartesianIndices(M)) do i
         isbox(M[i]) || return 0
         gps(i)
     end
 end
 
-function part1(M, I)
-    # println(repr(I))
-    M = deepcopy(M)
-    i = robot_pos(M)
-    # displ(M)
-    for (n, c) in enumerate(I)
-        ri = i
-        # println(c, ' ', n)
-        i = move!(M, i, c)
-        # o1 = displ(M)
-        # println("$ri -> $i")
 
-        # go(M, ri, dir(c))
-        # o2 = displ2(i)
-        # if strip(o1) != strip(o2)
-            # error("s1, s2 = $(repr(o1)), $(repr(o2))")
-        # end
-    end
-    answer(M)
-end
+### Part 2 ###
 
 function double(c::Char)
-    if c == '.'
-        return ".."
-    elseif c == 'O'
+    if c == 'O'
         return "[]"
-    elseif c == '#'
-        return "##"
     elseif c == '@'
         return "@."
     else
-        error("unhandled: '$c'")
+        return repeat(c, 2)
     end
 end
 
-function new_map(M)
-    # M′ = fill('.', size(M) .* 2)
-    # for i in CartesianIndices(M)
-    # end
-    # V = join.(eachrow(M))
-    # return Parsing._lines_into_matrix(V, double)
-
+function double(M::Matrix{Char})
     V = String[]
     io = IOBuffer()
     for row in eachrow(M)
@@ -303,69 +152,24 @@ function new_map(M)
     return Parsing._lines_into_matrix(V)
 end
 
-function answer2(M)
-    # println(repr(M))
-    sum(CartesianIndices(M)) do i
-        # println(i)
-        # println(M[i])
-        M[i] == '[' || return 0
-        gps(i)
-    end
-end
-
-function indices_to_move(M, si, d)
-    Q = Queue{Index}()
-    S = Set{Index}()
-    push!(Q, si)
-    while !isempty(Q)
-        i = dequeue!(Q)
-        j = i + d
-        if M[j] ∈ "[]"
-            enqueue!(M, j)
-
-            # check if there's another box to be pushed
-            if M[j] == '['
-                error("todo")
-            elseif M[j] == ']'
-            else
-                error("unreachable")
-            end
-        end
-    end
-end
-
-struct Box
-    left::Index
-    right::Index
-end
-
-function boxes_to_move(M, b::Box, d)
-    Q = Queue{Index}()
-    S = Seen{Index}()
-    enqueue!(Q, b.left)
-    enqueue!(Q, b.right)
-    while !isempty(Q)
-        # b =
-        # TODO
-    end
-end
-
-function moveboxes!(M, b::Box, d, walls, boxes)
-    # TODO
-end
-
-# adapted from https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24z445.
-# help from https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24swhr/
-function move2!(M, i, d)
+# Recursively move in direction d; adapted from:
+# <https://www.reddit.com/r/adventofcode/comments/1hele8m/comment/m24z445/>
+#
+# Returns a boolean of whether or not it can move
+#
+# Because this function modifies the input matrix, it's a good idea to copy
+# the matrix first before checking, in case you can't move and have to
+# roll back the change.
+function simulate!(M::Matrix{Char}, i::Index, d::Direction)
     j = i + d
     if M[j] == '#'
         return false
     elseif M[j] == '['
-        if  !move2!(M, j + INDEX_RIGHT, d) || !move2!(M, j, d)
+        if  !simulate!(M, j + INDEX_RIGHT, d) || !simulate!(M, j, d)
             return false
         end
     elseif M[j] == ']'
-        if !move2!(M, j + INDEX_LEFT, d) || !move2!(M, j, d)
+        if !simulate!(M, j + INDEX_LEFT, d) || !simulate!(M, j, d)
             return false
         end
     end
@@ -374,86 +178,44 @@ function move2!(M, i, d)
     return true
 end
 
-function mkbox(M, i)
-    c = M[i]
-    @assert c ∈ "[]"
-    if c == '['
-        return Box(i, i + INDEX_RIGHT)
-    elseif c == ']'
-        return Box(i + INDEX_LEFT, i)
+# Apply simulation if the move is allowed
+function move2!(M::Matrix{Char}, i::Index, d::Direction)
+    M′ = deepcopy(M)
+    if simulate!(M′, i, d)
+        copyto!(M, M′)
+        return i + d
     else
-        error("unreachable")
+        return i
     end
 end
 
 function part2(M, I)
-    M = deepcopy(M)
-    M = new_map(M)
-    displ(M)
-
-    # Get locations
-    walls = Set{Index}()
-    boxes = Set{Box}()
-    for i in CartesianIndices(M)
-        c = M[i]
-        if c == '#'
-            push!(walls, i)
-        elseif c ∈ "[]"
-            push!(boxes, mkbox(M, i))
-        end
+    # Simulate robot movements as per instructions
+    M = double(M)
+    i = find_robot(M)
+    for d in I
+        i = move2!(M, i, d)
     end
 
-    i = robot_pos(M)
-    for (n, c) in enumerate(I)
-        println(c, ' ', n)
-        d = dir(c)
-
-        M′ = deepcopy(M)
-        if move2!(M, i, d)
-            i += d
-        else
-            M = M′
-        end
-
-        # displ(M)
-
-        #=
-
-
-
-        j = i + d
-        if j ∈ walls
-            continue
-        end
-        if M[j] == '.'
-            i = j
-            continue
-        end
-        @assert M[j] ∈ "[]"
-        b = mkbox(M, j)
-        i = moveboxes!(M, b, d, walls, boxes)
-        =#
+    # Sum together gps coordinates of each box after simulation
+    return sum(CartesianIndices(M)) do i
+        M[i] == '[' || return 0
+        gps(i)
     end
-    answer2(M)
 end
 
 function main()
-    data = parse_input("data15.txt")
-    # data = parse_input("data15.test.txt")
-    # data = parse_input("data15.test2.txt")
-    # data = parse_input("data15.test3.txt")
+    M, I = parse_input("data15.txt")
 
     # Part 1
-    part1_solution = part1(data...)
-    # @assert part1_solution ==
+    part1_solution = part1(deepcopy(M), I)
+    @assert part1_solution == 1463715
     println("Part 1: $part1_solution")
 
     # Part 2
-    part2_solution = part2(data...)
-    # @assert part2_solution ==
+    part2_solution = part2(deepcopy(M), I)
+    @assert part2_solution == 1481392 part2_solution
     println("Part 2: $part2_solution")
 end
 
 main()
-
-# 1453900 is too low
