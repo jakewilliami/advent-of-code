db32057a8cbd1d86f18ab0595bd1ed70b5da59e7
Jake Ireland <jakewilliami@icloud.com>
2024-12-24 08:41:38 +1300

    Clean up day 23 solution in Julia

--- 2024/day23/day23.jl
+++ 2024/day23/day23.jl
@@ -1,28 +1,36 @@
-using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
+# Another nice problem today.  We were given a list of connections between pairs
+# of computers.  We can simply construct this into an undirected graph using a
+# popular graph theory library and find the answer from that.
+#
+# In part 1, we had to find how many groups of three computers are each connected
+# to each other.  This is simple enough with a naïve algorithm, brute forcing all
+# groups of three.  (As per the problem, these trianges would only count if at least
+# one computer in the group started with the letter t.)
+#
+# In part 2, we had to find a similar subgroup of computers that are all connected
+# to one another (these are called cliques in graph theory), but we had to find the
+# *largest* clique.  I used a recursive algorithm that took 5 and a half hours to
+# run, but it gave me the right answer.
 
-function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    # S = strip(read(input_file, String))
-    L = strip.(readlines(input_file))
-    L′ = [Symbol.(Tuple(split(l, '-'))) for l in L]
-    @assert all(t -> length(t) == 2, L′)
-    # L = get_integers.(L)
-    return L′
-end
+using Graphs
+
+
+### Parse Input ###
 
 const Connection = NTuple{2, Symbol}
 
-using Graphs
-# import Graphs.SimpleGraphs: SimpleGraph
+function parse_input(input_file::String)
+    L = Tuple.(split.(strip.(readlines(input_file)), '-'))
+    L′ = Connection[Symbol.((a, b)) for (a, b) in L]
+    return L′
+end
 
+
+### Part 1 ###
+
+# Graphs.jl only supports graphs whose vertices are integers.  To account
+# for this and know what the findings of graph traversal means, we need to
+# know how the computers map to vertices
 function make_vertex_map(C::Vector{Connection})
     V = Dict{Symbol, Int}()
     i = 1
@@ -33,7 +41,7 @@ function make_vertex_map(C::Vector{Connection})
             i += 1
         end
     end
-    V
+    return V
 end
 
 function make_graph(C::Vector{Connection})
@@ -46,10 +54,9 @@ function make_graph(C::Vector{Connection})
         i, j = V[a], V[b]
         M[i, j] = M[j, i] = true
     end
-    # println(M)
 
     # Create graph from adj matrix
-    SimpleGraph(M)
+    return SimpleGraph(M)
 end
 
 function find_loops_of_three(G, V)
@@ -57,22 +64,15 @@ function find_loops_of_three(G, V)
 
     for v in vertices(G)
         neighbours = neighbors(G, v)
-        # println("v=$v, n=$neighbours")
-        #=if length(neighbours) == 2
-            a, b = neighbours
-            push!(loops, v, a, b)
-        end
-        continue=#
-        # println(neighbours)
+
+        # We have a clique of 3 if we find two neighbours of v who are
+        # also connected
         for i in 1:length(neighbours)
             for j in (i + 1):length(neighbours)
                 a, b = neighbours[i], neighbours[j]
-                # println("$a, $b, has_edge=$(has_edge(G, a, b))")
                 has_edge(G, a, b) || continue
-                @assert has_edge(G, v, a)
-                @assert has_edge(G, v, b)
-                # println("here")
-                # must start with t
+
+                # Must start with t
                 t = (V[v], V[a], V[b])
                 any(x -> startswith(String(x), 't'), t) || continue
                 push!(loops, Set(t))
@@ -80,64 +80,55 @@ function find_loops_of_three(G, V)
         end
     end
 
-    loops
+    return loops
 end
 
 function part1(data::Vector{Connection})
     G = make_graph(data)
-    # println(G)
     V = Dict{Int, Symbol}(reverse(p) for p in make_vertex_map(data))
     return length(find_loops_of_three(G, V))
 end
 
-# pretty sure these are called cliques in graph theory
-function find_largest_clique(G, V)
-    largest_clique = Set{Symbol}()
-    n = nv(G)
 
-    function backtrack(current_clique, candidates)
-        if length(current_clique) > length(largest_clique)
-            largest_clique = Set(current_clique)
-        end
+### Part 2 ###
 
-        isempty(candidates) && return
-        for v in candidates
-            new_clique = push!(copy(current_clique), V[v])
-            new_candidates = filter(u -> has_edge(G, v, u), candidates)
-            backtrack(new_clique, new_candidates)
-        end
-    end
-
-    for (i, v) in enumerate(vertices(G))
-        println("$i/$(length(vertices(G)))")
-        backtrack(Set((V[v],)), filter(u -> has_edge(G, v, u), vertices(G)))
-    end
-
-    return largest_clique
-end
-
-function password(V::Set{Symbol})
-    join(sort(String.(V)), ',')
-end
+password(V::Set{Symbol}) = join(sort(String.(V)), ',')
 
 function part2(data::Vector{Connection})
     G = make_graph(data)
     V = Dict{Int, Symbol}(reverse(p) for p in make_vertex_map(data))
-    return password(find_largest_clique(G, V))
+
+    # Find the maximal clique (a subset of vertices in the graph that are all
+    # connected to each other) by looking for k-cliques (using the clique percolation
+    # algorithm defined in Graphs.jl) starting at k = 0 and increasing k until there
+    # is just one maximal clique and the next k-clique doesn't exist.  This wouldn't
+    # work for all graphs, but it works in this problem because of how the inputs
+    # are structured.  Graphs.jl also has `maximal_cliques` but I couldn't get that
+    # to give me the correct output.
+    k = 0
+    while true
+        C = clique_percolation(G, k = k)
+        if length(C) == 1 && isempty(clique_percolation(G, k = k + 1))
+            return password(Set{Symbol}(V[i] for i in only(C)))
+        end
+        k += 1
+    end
 end
+
+
+### Main ###
 
 function main()
     data = parse_input("data23.txt")
-    # data = parse_input("data23.test.txt")
 
     # Part 1
     part1_solution = part1(data)
-    # @assert part1_solution ==
+    @assert part1_solution == 1046
     println("Part 1: $part1_solution")
 
     # Part 2
     part2_solution = part2(data)
-    # @assert part2_solution ==
+    @assert part2_solution == "de,id,ke,ls,po,sn,tf,tl,tm,uj,un,xw,yz"
     println("Part 2: $part2_solution")
 end
 
