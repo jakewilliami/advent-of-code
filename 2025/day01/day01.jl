01c231cb236f2e37ab2af2a104b00df53e1f343f
Jake Ireland <jakewilliami@icloud.com>
2025-12-01 23:35:26 +1300

    Clean up day 1 solution in Julia
    It's still a naïve solution as it's simple to brute force, but it can
    be optimised further.

--- 2025/day01/day01.jl
+++ 2025/day01/day01.jl
@@ -1,136 +1,81 @@
-#  ]add ~/projects/AdventOfCode Statistics LinearAlgebra Combinatorics DataStructures StatsBase IntervalSets OrderedCollections MultidimensionalTools
-# using AdventOfCode.Parsing, AdventOfCode.Multidimensional
-# using Base.Iterators
-# using Statistics
-# using LinearAlgebra
-# using Combinatorics
-# using DataStructures
-# using StatsBase
-# using IntervalSets
-# using OrderedCollections
-# using MultidimensionalTools
+# In this problem, we have a dial on a circle of numbers; sequentially 0 to 99.
+# Each line of the puzzle input is an instruction: a direction (left or right)
+# in which you rotate the dial, and the number of notches that the dial is rotated.
+#
+# In part one, after each instruction is actioned, we are to count the number of
+# times the dial falls on 0.  In part two, we are to count the number of times the
+# dial falls on zero, even in passing (during an instruction),
+#
+# My performance today wasn't great.  It was harder than I expected, even though
+# it was a simple problem.  The first problem I was getting wrong as I read the
+# following:
+#   the number of times the dial is left pointing at 0 after any rotation in the
+#   sequence
+#
+# I read this as the dial has to be "left-pointing" and "at 0", not left (stopped)
+# at zero!  So that lost quite a bit of time.  And then in part 2, I tried a "clever"
+# solution which didn't work because I'm dumb (premature optimisation and all that),
+# so then I refactored it into a naïve solution but I had some old code in the loop
+# that was erroneously counting up some of the time, so the answer was only off by
+# a small number.  Despite the poor performance, it was fun.
+
+
+### Parse Input ###
 
 function parse_input(input_file::String)
-    # M = readlines_into_char_matrix(input_file)
-    # S = strip(read(input_file, String))
     L = strip.(readlines(input_file))
-    L = [(l[1], parse(Int, l[2:end])) for l in L]
-    # L = get_integers.(L)
-    return L
+    return Tuple{Char, Int}[(x[1], parse(Int, x[2:end])) for x in L]
 end
 
-function part1(data)
-    res = 50
-    a = 0
-    for (i, (d, n)) in enumerate(data)
-        # done = false
-        if res == 0
-            a += 1
-            # done = true
-        end
-        if d == 'L'
-            res = mod(res - n, 100)
-        elseif d == 'R'
-            res = mod(res + n, 100)
-        end
-        # if d == 'L' && res == 0 && !done
-            # a += 1
-        # end
-        # println("$d $n -> $d $res")
 
-        # if i < length(data)
-            # next_d = data[i + 1][1]
-            # if res == 0 && next_d == 'L'
-                # a += 1
-            # end
-        # end
-    end
-    return a
-end
+### Part 1 ###
 
-function part2(data)
-    p  = 0x434C49434B
-    res = 50
-    a = 0
-    #=for (d, n) in data
-        h = false
-        if res == 0
-            a += 1
-            h = true
-        end
+function part1(data::Vector{Tuple{Char, Int}})
+    dial, res = 50, 0
+    modifiers = Dict{Char, Int}('L' => -1, 'R' => 1)
 
-        m = d == 'L' ? -1 : 1
-        # r = d == 'L' ? (res-1:-1:res-n+1) : (res+1:res+n-1)
-
-        t = 0
-        for n′ in r
-            if n′ == 0
-                t += 1
-                a += 1
-            end
-            # res = mod(n′, 100)
-        end
-
-        if d == 'L'
-            res = mod(res - n, 100)
-        elseif d == 'R'
-            res = mod(res + n, 100)
-        end
-
-        if res == 0
-            a += 1
-            h = true
-        end
-
-        println("$d $n - $res - $t - $h")
-    end
-    =#
     for (d, n) in data
-        h = false
-        if d == 'L'
-            # if res == 0
-                # a -= 1
-            # end
-
-            i = 0
-            while i < n
-                res = mod(res - 1, 100)
-                if res == 0
-                    a += 1
-                    h = true
-                end
-                i += 1
-            end
-        elseif d == 'R'
-
-            i = 0
-            while i < n
-                res = mod(res + 1, 100)
-                if res == 0
-                    a += 1
-                    h = true
-                end
-                i += 1
-            end
-        end
-        # println("$d $n - $res - $h - $a")
+        res += iszero(dial)
+        modifier = modifiers[d]
+        dial = mod(dial + n * modifier, 100)
     end
-    return a
+
+    return res
 end
+
+
+### Part 2 ###
+
+function part2(data::Vector{Tuple{Char, Int}})
+    dial, res = 50, 0
+    modifiers = Dict{Char, Int}('L' => -1, 'R' => 1)
+
+    for (d, n) in data
+        modifier = modifiers[d]
+        i = 0
+        while i < n
+            dial = mod(dial + modifier, 100)
+            res += iszero(dial)
+            i += 1
+        end
+    end
+    return res
+end
+
+
+### Main ###
 
 function main()
     data = parse_input("data01.txt")
-    # data = parse_input("data01.test.txt")
-    # println(data)
 
     # Part 1
     part1_solution = part1(data)
-    # @assert part1_solution ==
+    @assert part1_solution == 1118
     println("Part 1: $part1_solution")
 
     # Part 2
     part2_solution = part2(data)
-    # @assert part2_solution ==
+    @assert part2_solution == 6289
     println("Part 2: $part2_solution")
 end
 
